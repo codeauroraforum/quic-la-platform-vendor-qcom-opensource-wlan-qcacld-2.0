923225bbac468e42ea5ea6f33c3fa13cf351c754
Agarwal Ashish <ashishka@qca.qualcomm.com>
2014-03-30 22:45:11 -0700

    wlan: SME log level modification.
    Added logs which should get printed when some unexpected error
    happens. Modification in log level for SME.
    
    Change-Id: I089ecd52313481b049eba856e436a58075661e20
    CRs-Fixed: 640957

--- CORE/SME/src/csr/csrUtil.c
+++ CORE/SME/src/csr/csrUtil.c
@@ -3110,7 +3110,7 @@ eHalStatus csrValidateMCCBeaconInterval(tpAniSirGlobal pMac, tANI_U8 channelId,
                 break;
 
                 default :
-                    smsLog(pMac, LOG1, FL(" Persona not supported : %d"),currBssPersona);
+                    smsLog(pMac, LOGE, FL(" Persona not supported : %d"),currBssPersona);
                     return eHAL_STATUS_FAILURE;
             }
         }
@@ -6151,7 +6151,7 @@ v_CountryInfoSource_t source
         }
         else
         {
-            smsLog(pMac, LOGE, FL("  doesn't match country %c%c, status %d"), pCountry[0], pCountry[1], vosStatus);
+            smsLog(pMac, LOGW, FL(" Couldn't find domain for country code  %c%c"), pCountry[0], pCountry[1]);
             status = eHAL_STATUS_INVALID_PARAMETER;
         }
     }
