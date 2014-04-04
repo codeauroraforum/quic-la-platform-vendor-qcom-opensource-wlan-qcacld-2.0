5add2bd4723e943867a3843035e7348793e8f8ce
Akash Patel <akashp@codeaurora.org>
2014-04-03 19:18:49 -0700

    Release 1.3.6.08
    Release 1.3.6.08
    
    Change-Id: I6364944e384de1bdfbcbf9566266a48b3906103e
    (cherry picked from commit 97f21f2c0dfe9bc976ebe2a6f929a1d26e9b7ad2)

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            3
 #define QWLAN_VERSION_PATCH            6
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            07
+#define QWLAN_VERSION_BUILD            08
 
-#define QWLAN_VERSIONSTR               "1.3.6.07"
+#define QWLAN_VERSIONSTR               "1.3.6.08"
 
 #ifdef QCA_WIFI_2_0
 
