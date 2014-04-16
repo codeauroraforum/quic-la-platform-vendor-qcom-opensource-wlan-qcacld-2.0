8f873979661e27f2a3059d8903fd273ef653de73
Prashanth Bhatta <bhattap@qca.qualcomm.com>
2014-04-16 12:06:27 -0700

    qcacld: HDD: bus bandwidth enhancement
    Fixing/enhancing following items related to bus bandwidth request
        1. Increment TX counters which was missing in earlier
        implementation
        2. Consider all the adapters as well as TX and RX for
        computing bus bandwidth level as all the data packet goes
        through same bus irrespective of adapter or direction.
        3. Added ini configuration so that configuration can be made
        based on platform requirement.
    
    Change-Id: I3753c0778fcda2c988dc3ff6e59abba4d29622a8
    CRs-fixed: 652692

--- CORE/HDD/inc/wlan_hdd_cfg.h
+++ CORE/HDD/inc/wlan_hdd_cfg.h
@@ -2417,6 +2417,27 @@ This feature requires the dependent cfg.ini "gRoamPrefer5GHz" set to 1 */
 #define CFG_ROAMING_DFS_CHANNEL_MAX                 (1)
 #define CFG_ROAMING_DFS_CHANNEL_DEFAULT             (0)
 
+#ifdef MSM_PLATFORM
+#define CFG_BUS_BANDWIDTH_HIGH_THRESHOLD           "gBusBandwidthHighThreshold"
+#define CFG_BUS_BANDWIDTH_HIGH_THRESHOLD_DEFAULT   ( 40000 )
+#define CFG_BUS_BANDWIDTH_HIGH_THRESHOLD_MIN       ( 10000 )
+#define CFG_BUS_BANDWIDTH_HIGH_THRESHOLD_MAX       ( 4294967295UL )
+
+#define CFG_BUS_BANDWIDTH_MEDIUM_THRESHOLD         "gBusBandwidthMediumThreshold"
+#define CFG_BUS_BANDWIDTH_MEDIUM_THRESHOLD_DEFAULT ( 10000 )
+#define CFG_BUS_BANDWIDTH_MEDIUM_THRESHOLD_MIN     ( 3000 )
+#define CFG_BUS_BANDWIDTH_MEDIUM_THRESHOLD_MAX     ( 40000 )
+
+#define CFG_BUS_BANDWIDTH_LOW_THRESHOLD            "gBusBandwidthLowThreshold"
+#define CFG_BUS_BANDWIDTH_LOW_THRESHOLD_DEFAULT    ( 3000 )
+#define CFG_BUS_BANDWIDTH_LOW_THRESHOLD_MIN        ( 0 )
+#define CFG_BUS_BANDWIDTH_LOW_THRESHOLD_MAX        ( 10000 )
+
+#define CFG_BUS_BANDWIDTH_COMPUTE_INTERVAL         "gBusBandwidthComputeInterval"
+#define CFG_BUS_BANDWIDTH_COMPUTE_INTERVAL_DEFAULT ( 3000 )
+#define CFG_BUS_BANDWIDTH_COMPUTE_INTERVAL_MIN     ( 1000 )
+#define CFG_BUS_BANDWIDTH_COMPUTE_INTERVAL_MAX     ( 10000 )
+#endif /* MSM_PLATFORM */
 /*---------------------------------------------------------------------------
   Type declarations
   -------------------------------------------------------------------------*/
@@ -2926,6 +2947,12 @@ typedef struct
    v_BOOL_t                    debugP2pRemainOnChannel;
 
    v_BOOL_t                    enablePacketLog;
+#ifdef MSM_PLATFORM
+   v_U32_t                     busBandwidthHighThreshold;
+   v_U32_t                     busBandwidthMediumThreshold;
+   v_U32_t                     busBandwidthLowThreshold;
+   v_U32_t                     busBandwidthComputeInterval;
+#endif /* MSM_PLATFORM */
 } hdd_config_t;
 /*---------------------------------------------------------------------------
   Function declarations and documenation
--- CORE/HDD/src/wlan_hdd_cfg.c
+++ CORE/HDD/src/wlan_hdd_cfg.c
@@ -3362,6 +3362,35 @@ REG_VARIABLE( CFG_SAP_MAX_NO_PEERS, WLAN_PARAM_Integer,
                  CFG_ENABLE_PACKET_LOG_MIN,
                  CFG_ENABLE_PACKET_LOG_MAX ),
 
+#ifdef MSM_PLATFORM
+   REG_VARIABLE( CFG_BUS_BANDWIDTH_HIGH_THRESHOLD, WLAN_PARAM_Integer,
+                 hdd_config_t, busBandwidthHighThreshold,
+                 VAR_FLAGS_OPTIONAL | VAR_FLAGS_RANGE_CHECK_ASSUME_DEFAULT,
+                 CFG_BUS_BANDWIDTH_HIGH_THRESHOLD_DEFAULT,
+                 CFG_BUS_BANDWIDTH_HIGH_THRESHOLD_MIN,
+                 CFG_BUS_BANDWIDTH_HIGH_THRESHOLD_MAX ),
+
+   REG_VARIABLE( CFG_BUS_BANDWIDTH_MEDIUM_THRESHOLD, WLAN_PARAM_Integer,
+                 hdd_config_t, busBandwidthMediumThreshold,
+                 VAR_FLAGS_OPTIONAL | VAR_FLAGS_RANGE_CHECK_ASSUME_DEFAULT,
+                 CFG_BUS_BANDWIDTH_MEDIUM_THRESHOLD_DEFAULT,
+                 CFG_BUS_BANDWIDTH_MEDIUM_THRESHOLD_MIN,
+                 CFG_BUS_BANDWIDTH_MEDIUM_THRESHOLD_MAX ),
+
+   REG_VARIABLE( CFG_BUS_BANDWIDTH_LOW_THRESHOLD, WLAN_PARAM_Integer,
+                 hdd_config_t, busBandwidthLowThreshold,
+                 VAR_FLAGS_OPTIONAL | VAR_FLAGS_RANGE_CHECK_ASSUME_DEFAULT,
+                 CFG_BUS_BANDWIDTH_LOW_THRESHOLD_DEFAULT,
+                 CFG_BUS_BANDWIDTH_LOW_THRESHOLD_MIN,
+                 CFG_BUS_BANDWIDTH_LOW_THRESHOLD_MAX ),
+
+   REG_VARIABLE( CFG_BUS_BANDWIDTH_COMPUTE_INTERVAL, WLAN_PARAM_Integer,
+                 hdd_config_t, busBandwidthComputeInterval,
+                 VAR_FLAGS_OPTIONAL | VAR_FLAGS_RANGE_CHECK_ASSUME_DEFAULT,
+                 CFG_BUS_BANDWIDTH_COMPUTE_INTERVAL_DEFAULT,
+                 CFG_BUS_BANDWIDTH_COMPUTE_INTERVAL_MIN,
+                 CFG_BUS_BANDWIDTH_COMPUTE_INTERVAL_MAX),
+#endif
 };
 
 /*
@@ -3765,6 +3794,19 @@ static void print_hdd_cfg(hdd_context_t *pHddCtx)
   VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO_HIGH, "Name = [gMaxOffloadPeers] Value = [%u] ",pHddCtx->cfg_ini->apMaxOffloadPeers);
   VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO_HIGH, "Name = [overrideCountryCode] Value = [%s] ",pHddCtx->cfg_ini->overrideCountryCode);
   VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO_HIGH, "Name = [gAllowDFSChannelRoam] Value = [%u] ",pHddCtx->cfg_ini->allowDFSChannelRoam);
+
+  VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO_HIGH,
+          "Name = [gBusBandwidthHighThreshold] Value = [%u] ",
+          pHddCtx->cfg_ini->busBandwidthHighThreshold);
+  VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO_HIGH,
+          "Name = [gBusBandwidthMediumThreshold] Value = [%u] ",
+          pHddCtx->cfg_ini->busBandwidthMediumThreshold);
+  VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO_HIGH,
+          "Name = [gBusBandwidthLowThreshold] Value = [%u] ",
+          pHddCtx->cfg_ini->busBandwidthLowThreshold);
+  VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO_HIGH,
+          "Name = [gbusBandwidthComputeInterval] Value = [%u] ",
+          pHddCtx->cfg_ini->busBandwidthComputeInterval);
 }
 
 
