68b45ea1df4a043babf3c8a825bb4332e9a3a7cf
Rajeev Kumar <rajekuma@qca.qualcomm.com>
2014-04-28 19:48:31 -0700

    qcacld: Add a build flag to enable/disable FW log
    Add a build flag to enable/disable FW logging through
    CFG INI
    
    Change-Id: I5008abfa11afb0aa2a3de0b9f546c182657f6a9d
    CRs-fixed: 655931

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -8329,6 +8329,8 @@ hdd_adapter_t* hdd_open_adapter( hdd_context_t *pHddCtx, tANI_U8 session_type,
 
 #endif
 
+#ifdef CONFIG_FW_LOGS_BASED_ON_INI
+
   /* Enable FW logs based on INI configuration */
   if ((VOS_FTM_MODE != vos_get_conparam()) &&
              (pHddCtx->cfg_ini->enableFwLogType))
@@ -8388,6 +8390,8 @@ hdd_adapter_t* hdd_open_adapter( hdd_context_t *pHddCtx, tANI_U8 session_type,
          count += 2;
      }
   }
+
+#endif
 
 
    return pAdapter;
