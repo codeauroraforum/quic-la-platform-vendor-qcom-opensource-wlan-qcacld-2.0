3d2ae7f9d7a7ad34366380fcfd58b60ef5821fb3
Mushtaq A Mujale <mmujale@qca.qualcomm.com>
2014-05-20 17:53:46 -0700

    qcacld: Pause/Unpause pdev during suspend/resume.
    Make sure no new downloads start by calling ol_txrx_pdev_pause,
    ol_txrx_get_tx_pending only makes sure there are no in-progress
    tranmissions.
    
    Change-Id: Idb71176462302f633909c35038c0a24f87937ace
    CRs-Fixed: 661532

--- CORE/CLD_TXRX/TXRX/ol_tx_queue.c
+++ CORE/CLD_TXRX/TXRX/ol_tx_queue.c
@@ -554,6 +554,26 @@ ol_txrx_vdev_flush(ol_txrx_vdev_handle vdev)
     }
 }
 
+void
+ol_txrx_pdev_pause(ol_txrx_pdev_handle pdev)
+{
+    struct ol_txrx_vdev_t *vdev = NULL, *tmp;
+
+    TAILQ_FOREACH_SAFE(vdev, &pdev->vdev_list, vdev_list_elem, tmp) {
+        ol_txrx_vdev_pause(vdev);
+    }
+}
+
+void
+ol_txrx_pdev_unpause(ol_txrx_pdev_handle pdev)
+{
+    struct ol_txrx_vdev_t *vdev = NULL, *tmp;
+
+    TAILQ_FOREACH_SAFE(vdev, &pdev->vdev_list, vdev_list_elem, tmp) {
+        ol_txrx_vdev_unpause(vdev);
+    }
+}
+
 #endif // defined(CONFIG_HL_SUPPORT) || defined(QCA_SUPPORT_TXRX_VDEV_PAUSE_LL)
 
 /*--- LL tx throttle queue code --------------------------------------------*/
