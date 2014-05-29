582679321690826796c7ba63bca78c2c19ce36f0
Akash Patel <c_akashp@qca.qualcomm.com>
2014-05-29 16:27:54 -0700

    Cafstaging Release 1.0.0.117
    Cafstaging Release 1.0.0.117
    
    Change-Id: I3c853724cfec18d7bcd085946939ebe7cb3f56a9
    CRs-Fixed: 672907

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -41,10 +41,10 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MAJOR            4
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            "LE"
-#define QWLAN_VERSION_BUILD            20
+#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_BUILD            117
 
-#define QWLAN_VERSIONSTR               "4.0.0.20LE"
+#define QWLAN_VERSIONSTR               "1.0.0.117"
 
 #ifdef QCA_WIFI_2_0
 
