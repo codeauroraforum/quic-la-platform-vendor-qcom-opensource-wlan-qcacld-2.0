3db7de6653d5834ca5500f18bdc8d9bf469e638f
Akash Patel <c_akashp@qca.qualcomm.com>
2014-06-02 15:07:24 -0700

    Release 1.3.6.54
    Release 1.3.6.54
    
    Change-Id: Ia8870521f19ba713fa29b22930845606b8958a38
    CRs-FIxed: 674065

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            3
 #define QWLAN_VERSION_PATCH            6
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            53
+#define QWLAN_VERSION_BUILD            54
 
-#define QWLAN_VERSIONSTR               "1.3.6.53"
+#define QWLAN_VERSIONSTR               "1.3.6.54"
 
 #ifdef QCA_WIFI_2_0
 
