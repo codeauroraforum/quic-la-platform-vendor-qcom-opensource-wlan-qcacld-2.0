ada7a6c0bd33a13c81038beedd20f60d6b592751
Ganesh Babu Kumaravel <kganesh@qti.qualcomm.com>
2014-06-16 15:49:43 +0530

    qcacld:configure powersave parameters
    Dynamic DTIM and Modulated DTIM parameters need to passed from
    umac inorder to configure these params during system suspend case.
    
    Inaddition pass last beacon dtim period also which is required
    to compute the listen interval
    
    Change-Id: Ia451835051a4aea8fc81195458356a1332626d37
    CRs-Fixed: 674826
    (cherry picked from commit 1fa0b0fc8eadb51837fbe8f3bf03f965a2a4b0d3)

--- CORE/MAC/inc/aniGlobal.h
+++ CORE/MAC/inc/aniGlobal.h
@@ -990,6 +990,10 @@ typedef struct sMacOpenParameters
     tANI_U16 maxBssId;
     tANI_U32 frameTransRequired;
     tANI_U8 powersaveOffloadEnabled;
+    /* Powersave Parameters */
+    tANI_U8 staMaxLIModDtim;
+    tANI_U8 staModDtim;
+    tANI_U8 staDynamicDtim;
     tDriverType  driverType;
     tANI_U8 maxWoWFilters;
     tANI_U8 wowEnable;
