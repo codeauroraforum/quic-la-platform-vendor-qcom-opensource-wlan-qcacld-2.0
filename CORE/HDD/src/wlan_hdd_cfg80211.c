f7ee31fcf4444b355567899b4dcf3940747a929b
Satish Singh <ssing@codeaurora.org>
2014-06-20 16:37:08 -0700

    Disable SAP 11ac mode for VHT80 unsupported domains.
    When SAP comes with VHT80 unsupported country codes
    then fall back the SAP mode to 11n if the mode is 11ac.
    
    Change-Id: I8bdde153cf7290e8c7ac7204565ebb0302951646
    CRs-Fixed: 659673

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -2524,9 +2524,10 @@ static int wlan_hdd_cfg80211_start_bss(hdd_adapter_t *pHostapdAdapter,
         else
             pConfig->SapHw_mode = eSAP_DOT11_MODE_11ac;
 
-        /* Disable VHT support in 2.4 GHz band */
-        if (pConfig->channel <= 14 &&
-            (WLAN_HDD_GET_CTX(pHostapdAdapter))->cfg_ini->enableVhtFor24GHzBand == FALSE)
+        if (((pConfig->channel <= 14) &&
+            (WLAN_HDD_GET_CTX(pHostapdAdapter)->cfg_ini->enableVhtFor24GHzBand
+                                                                 == FALSE)) ||
+            (WLAN_HDD_GET_CTX(pHostapdAdapter)->isVHT80Allowed == FALSE))
         {
             pConfig->SapHw_mode = eSAP_DOT11_MODE_11n;
         }
