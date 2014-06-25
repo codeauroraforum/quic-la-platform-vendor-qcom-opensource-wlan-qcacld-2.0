8512d282441f82b3523ed95012dd95c41f623a41
Satish Singh <ssing@codeaurora.org>
2014-06-25 16:19:28 -0700

    Release 1.3.6.62A
    Release 1.3.6.62A
    
    Change-Id: I250911ae542cb2c4c77567301e85fca18feed1a0
    CRs-Fixed: 684903

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -41,10 +41,10 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MAJOR            1
 #define QWLAN_VERSION_MINOR            3
 #define QWLAN_VERSION_PATCH            6
-#define QWLAN_VERSION_EXTRA           ""
+#define QWLAN_VERSION_EXTRA           "A"
 #define QWLAN_VERSION_BUILD            62
 
-#define QWLAN_VERSIONSTR               "1.3.6.62"
+#define QWLAN_VERSIONSTR               "1.3.6.62A"
 
 #ifdef QCA_WIFI_2_0
 
