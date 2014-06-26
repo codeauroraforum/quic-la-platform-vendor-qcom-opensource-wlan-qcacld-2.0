9e9ee00b1e944cfb790a6204007d9805680fcda9
Mahesh Kumar Kalikot Veetil <mkalikot@codeaurora.org>
2014-06-26 16:12:22 -0700

    Cafstaging Release 1.0.0.132
    Cafstaging Release 1.0.0.132
    
    Change-Id: I34c5c04c0f0abcb2b6ebcb64870faaf24367e2bf
    CRs-Fixed: 684605

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -41,10 +41,10 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MAJOR            4
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            "LE"
-#define QWLAN_VERSION_BUILD            29
+#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_BUILD            132
 
-#define QWLAN_VERSIONSTR               "4.0.0.29LE"
+#define QWLAN_VERSIONSTR               "1.0.0.132"
 
 #ifdef QCA_WIFI_2_0
 
