451e70cb916e482696261c6b45832233e5862ad7
krunal soni <ksoni@codeaurora.org>
2014-06-27 17:02:25 -0700

    qcacld: add additional information in existing logs.
    Few logs are printing the same string in different cases.
    In failure cases it is becoming difficult to find out from where
    it is getting printed. So adding function names to these logs.
    
    Change-Id: I97d97712d9a5d3dcdf31164ccb63b13a180d3714
    CRs-Fixed: 684680

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -2613,7 +2613,8 @@ static int wlan_hdd_cfg80211_start_bss(hdd_adapter_t *pHostapdAdapter,
     if (!VOS_IS_STATUS_SUCCESS(status))
     {
         VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
-                 ("ERROR: HDD vos wait for single_event failed!!"));
+                 ("%s: ERROR: HDD vos wait for single_event failed!!"),
+                 __func__);
         smeGetCommandQStatus(hHal);
         VOS_ASSERT(0);
         return -EINVAL;
@@ -2883,7 +2884,8 @@ static int wlan_hdd_cfg80211_stop_ap (struct wiphy *wiphy,
                 if (!VOS_IS_STATUS_SUCCESS(status))
                 {
                     VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
-                             ("ERROR: HDD vos wait for single_event failed!!"));
+                             ("%s: ERROR: HDD vos wait for single_event failed!!"),
+                             __func__);
                     VOS_ASSERT(0);
                 }
             }
