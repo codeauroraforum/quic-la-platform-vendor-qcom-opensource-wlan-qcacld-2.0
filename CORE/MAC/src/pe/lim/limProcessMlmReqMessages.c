3303e136f9e2c62078d7a68232dbcc1aab4b7ff2
Krishna Kumaar Natarajan <kknatara@qca.qualcomm.com>
2014-07-29 18:25:44 -0700

    qcacld: wlan: Fix to set correct phy mode
    As of now, if we have no channel bonding and the AP is 11G only
    we still set the phy mode to 11NGHT20.
    
    This change set will fix the incorrect phy mode setting by passing
    the dot11_mode to WMA layer
    
    Change-Id: Ia65c75aa911a316d091a9ece779b3376c7ee0602
    CRs-Fixed: 701474

--- CORE/MAC/src/pe/lim/limProcessMlmReqMessages.c
+++ CORE/MAC/src/pe/lim/limProcessMlmReqMessages.c
@@ -1753,6 +1753,9 @@ limMlmAddBss (
     // A global counter (dialog token) is required to keep track of
     // all PE <-> HAL communication(s)
     //
+    pAddBssParams->dot11_mode = psessionEntry->dot11mode;
+    limLog(pMac, LOG2, FL("dot11_mode:%d"), pAddBssParams->dot11_mode);
+
     msgQ.type       = WDA_ADD_BSS_REQ;
     msgQ.reserved   = 0;
     msgQ.bodyptr    = pAddBssParams;
