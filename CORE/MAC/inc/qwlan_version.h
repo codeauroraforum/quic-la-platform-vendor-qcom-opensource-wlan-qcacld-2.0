e41a8e23239cfe7572f48cb9d1dff80f3533392d
Vishwajith Upendra <vishwaji@codeaurora.org>
2014-09-13 13:39:50 -0700

    Cafstaging Release 1.0.0.190
    Cafstaging Release 1.0.0.190
    
    Change-Id: Ic3f5dcdf619455cdbbc647f9a10ef8de2e41ba05
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -38,13 +38,13 @@ BRIEF DESCRIPTION:
 
 ===========================================================================*/
 
-#define QWLAN_VERSION_MAJOR            4
+#define QWLAN_VERSION_MAJOR            1
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            "LE"
-#define QWLAN_VERSION_BUILD            50
+#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_BUILD            190
 
-#define QWLAN_VERSIONSTR               "4.0.0.50LE"
+#define QWLAN_VERSIONSTR               "1.0.0.190"
 
 #ifdef QCA_WIFI_2_0
 
