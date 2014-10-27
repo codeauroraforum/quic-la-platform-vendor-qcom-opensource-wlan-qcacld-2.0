110be57b9d4403d87a6f1275a17ddf03e5265925
Krunal Soni <ksoni@qca.qualcomm.com>
2014-10-27 12:20:42 -0700

    qcacld:PMF:Fix the minimum allowed time for pmfsaquery retry interval
    Driver can create the timer up to minimum of 10ms. Anything less than
    that is not permitted.
    
    With this fix the user can configure min value till 10ms. If user
    tried to configure less than that then driver will pick the default
    value.
    
    Change-Id: I96e2cc7c49251d93589870d255ef1eb7d64706f9
    CRs-Fixed: 729143

--- CORE/MAC/inc/wniCfgAp.h
+++ CORE/MAC/inc/wniCfgAp.h
@@ -1,29 +1,29 @@
 /*
-  * Copyright (c) 2011-2014 The Linux Foundation. All rights reserved.
-  *
-  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
-  *
-  *
-  * Permission to use, copy, modify, and/or distribute this software for
-  * any purpose with or without fee is hereby granted, provided that the
-  * above copyright notice and this permission notice appear in all
-  * copies.
-  *
-  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
-  * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
-  * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
-  * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
-  * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
-  * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
-  * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-  * PERFORMANCE OF THIS SOFTWARE.
-  */
+ * Copyright (c) 2011-2014 The Linux Foundation. All rights reserved.
+ *
+ * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
+ *
+ *
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all
+ * copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
 
- /*
-  * This file was originally distributed by Qualcomm Atheros, Inc.
-  * under proprietary terms before Copyright ownership was assigned
-  * to the Linux Foundation.
-  */
+/*
+ * This file was originally distributed by Qualcomm Atheros, Inc.
+ * under proprietary terms before Copyright ownership was assigned
+ * to the Linux Foundation.
+ */
 
 /*
  * DO NOT EDIT - This file is generated automatically
@@ -2533,11 +2533,11 @@
 #define WNI_CFG_PMF_SA_QUERY_MAX_RETRIES_APMAX    20
 #define WNI_CFG_PMF_SA_QUERY_MAX_RETRIES_APDEF    5
 
-#define WNI_CFG_PMF_SA_QUERY_RETRY_INTERVAL_STAMIN    0
+#define WNI_CFG_PMF_SA_QUERY_RETRY_INTERVAL_STAMIN    10
 #define WNI_CFG_PMF_SA_QUERY_RETRY_INTERVAL_STAMAX    2000
 #define WNI_CFG_PMF_SA_QUERY_RETRY_INTERVAL_STADEF    200
 
-#define WNI_CFG_PMF_SA_QUERY_RETRY_INTERVAL_APMIN    0
+#define WNI_CFG_PMF_SA_QUERY_RETRY_INTERVAL_APMIN    10
 #define WNI_CFG_PMF_SA_QUERY_RETRY_INTERVAL_APMAX    2000
 #define WNI_CFG_PMF_SA_QUERY_RETRY_INTERVAL_APDEF    200
 
