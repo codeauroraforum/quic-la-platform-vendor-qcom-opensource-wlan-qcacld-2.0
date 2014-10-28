902b23b62548607d6490da8761d461e8a76e86c6
Komal Seelam <kseelam@qti.qualcomm.com>
2014-10-28 16:45:34 -0700

    qcacld-2.0: Fix Long Driver Load Time.
    Due to aggressive Krait PS, the driver load time is high.
    Fix by voting APPS not to enter Idle/Standalone PS during driver load.
    
    Change-Id: I5b2c84fa5e24487cb2750db83ee79c5a4f338642
    CRs-Fixed: 747649

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -164,7 +164,7 @@ void hdd_ch_avoid_cb(void *hdd_context,void *indi_param);
 #define MEMORY_DEBUG_STR ""
 #endif
 
-
+#define DISABLE_KRAIT_IDLE_PS_VAL   200
 #ifdef IPA_UC_OFFLOAD
 /* If IPA UC data path is enabled, target should reserve extra tx descriptors
  * for IPA WDI data path.
@@ -12485,6 +12485,34 @@ success:
    return 0;
 }
 
+/*
+ * In BMI Phase we are only sending small chunk (256 bytes) of the FW image at
+ * a time, and wait for the completion interrupt to start the next transfer.
+ * During this phase, the KRAIT is entering IDLE/StandAlone(SA) Power Save(PS).
+ * The delay incurred for resuming from IDLE/SA PS is huge during driver load.
+ * So prevent APPS IDLE/SA PS during driver load for reducing interrupt latency.
+ */
+
+#ifdef CONFIG_CNSS
+static inline void hdd_request_pm_qos(int val)
+{
+   cnss_request_pm_qos(val);
+}
+
+static inline void hdd_remove_pm_qos(void)
+{
+   cnss_remove_pm_qos();
+}
+#else
+static inline void hdd_request_pm_qos(int val)
+{
+}
+
+static inline void hdd_remove_pm_qos(void)
+{
+}
+#endif
+
 /**---------------------------------------------------------------------------
 
   \brief hdd_driver_init() - Core Driver Init Function
@@ -12512,6 +12540,13 @@ static int hdd_driver_init( void)
 
    vos_wake_lock_init(&wlan_wake_lock, "wlan");
    hdd_prevent_suspend();
+   /*
+    * The Krait is going to Idle/Stand Alone Power Save
+    * more aggressively which is resulting in the longer driver load time.
+    * The Fix is to not allow Krait to enter Idle Power Save during driver load.
+    */
+
+   hdd_request_pm_qos(DISABLE_KRAIT_IDLE_PS_VAL);
 #ifdef HDD_TRACE_RECORD
    MTRACE(hddTraceInit());
 #endif
@@ -12525,6 +12560,7 @@ static int hdd_driver_init( void)
          ret_status =  epping_driver_init(con_mode, &wlan_wake_lock,
                           WLAN_MODULE_NAME);
          if (ret_status < 0) {
+            hdd_remove_pm_qos();
             hdd_allow_suspend();
             vos_wake_lock_destroy(&wlan_wake_lock);
          }
@@ -12535,6 +12571,7 @@ static int hdd_driver_init( void)
          ret_status = epping_driver_init(hdd_get_conparam(),
                          &wlan_wake_lock, WLAN_MODULE_NAME);
          if (ret_status < 0) {
+            hdd_remove_pm_qos();
             hdd_allow_suspend();
             vos_wake_lock_destroy(&wlan_wake_lock);
          }
@@ -12578,6 +12615,7 @@ static int hdd_driver_init( void)
            ret_status = 0;
    }
 
+   hdd_remove_pm_qos();
    hdd_allow_suspend();
 
    if (ret_status) {
