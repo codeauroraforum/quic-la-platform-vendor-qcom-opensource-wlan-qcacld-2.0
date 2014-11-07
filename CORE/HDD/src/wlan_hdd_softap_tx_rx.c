5ef72deb78298869b724ee18f161dd05f37e1828
Satish Singh <ssing@codeaurora.org>
2014-11-06 19:13:50 -0800

    qcacld: HDD: bus bandwidth enhancement
    Fixing/enhancing following items related to bus bandwidth request
        1. Increment TX counters which was missing in earlier
        implementation
        2. Consider all the adapters as well as TX and RX for
        computing bus bandwidth level as all the data packet goes
        through same bus irrespective of adapter or direction.
        3. Added ini configuration so that configuration can be
        made based on platform requirement.
    
    Change-Id: I3753c0778fcda2c988dc3ff6e59abba4d29622a8
    CRs-fixed: 652692

--- CORE/HDD/src/wlan_hdd_softap_tx_rx.c
+++ CORE/HDD/src/wlan_hdd_softap_tx_rx.c
@@ -561,9 +561,7 @@ int hdd_softap_hard_start_xmit(struct sk_buff *skb, struct net_device *dev)
 
    pDestMacAddress = (v_MACADDR_t*)skb->data;
 
-   ++pAdapter->hdd_stats.hddTxRxStats.txXmitCalled;
-
-   VOS_TRACE( VOS_MODULE_ID_HDD_SOFTAP, VOS_TRACE_LEVEL_INFO,
+   VOS_TRACE( VOS_MODULE_ID_HDD_SAP_DATA, VOS_TRACE_LEVEL_INFO,
               "%s: enter", __func__);
 
    if (vos_is_macaddr_broadcast( pDestMacAddress ) ||
@@ -663,6 +661,8 @@ int hdd_softap_hard_start_xmit(struct sk_buff *skb, struct net_device *dev)
       }
    }
 #endif /* QCA_PKT_PROTO_TRACE */
+   pAdapter->stats.tx_bytes += skb->len;
+   ++pAdapter->stats.tx_packets;
 
    if (WLANTL_SendSTA_DataFrame((WLAN_HDD_GET_CTX(pAdapter))->pvosContext,
                                  STAId, skb
@@ -676,10 +676,6 @@ int hdd_softap_hard_start_xmit(struct sk_buff *skb, struct net_device *dev)
         goto drop_pkt;
    }
 
-   ++pAdapter->hdd_stats.hddTxRxStats.txXmitClassifiedAC[ac];
-   ++pAdapter->hdd_stats.hddTxRxStats.txXmitQueued;
-   ++pAdapter->hdd_stats.hddTxRxStats.txXmitQueuedAC[ac];
-
    dev->trans_start = jiffies;
 
    VOS_TRACE( VOS_MODULE_ID_HDD_SOFTAP, VOS_TRACE_LEVEL_INFO_LOW, "%s: exit", __func__);
@@ -689,8 +685,6 @@ int hdd_softap_hard_start_xmit(struct sk_buff *skb, struct net_device *dev)
 drop_pkt:
 
    ++pAdapter->stats.tx_dropped;
-   ++pAdapter->hdd_stats.hddTxRxStats.txXmitDropped;
-   ++pAdapter->hdd_stats.hddTxRxStats.txXmitDroppedAC[ac];
    kfree_skb(skb);
 
    return NETDEV_TX_OK;
