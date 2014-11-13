e5642e8b5750f6021efca562a9e0ffa572ed3655
Nandini Suresh <snandini@codeaurora.org>
2014-11-13 09:31:45 -0800

    Cafstaging Release 1.0.0.233
    Cafstaging Release 1.0.0.233
    
    Change-Id: Ie856bb294f037cafe19cdd973b45dac9ec0764c1
    ..

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -38,13 +38,13 @@ BRIEF DESCRIPTION:
 
 ===========================================================================*/
 
-#define QWLAN_VERSION_MAJOR            4
+#define QWLAN_VERSION_MAJOR            1
 #define QWLAN_VERSION_MINOR            0
-#define QWLAN_VERSION_PATCH            2
-#define QWLAN_VERSION_EXTRA             "LE"
-#define QWLAN_VERSION_BUILD            18
+#define QWLAN_VERSION_PATCH            0
+#define QWLAN_VERSION_EXTRA             ""
+#define QWLAN_VERSION_BUILD            233
 
-#define QWLAN_VERSIONSTR               "4.0.2.18LE"
+#define QWLAN_VERSIONSTR               "1.0.0.233"
 
 #ifdef QCA_WIFI_2_0
 
