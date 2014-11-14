d674f121a7bfe12c89519c1177f132e1052b9dcb
Kiran Kumar Lokere <klokere@qca.qualcomm.com>
2014-11-14 14:18:13 -0800

    qcacld: Fix the issue in CB mode selection in 11ac mode
    Check the VHT channel width and phy mode in CB mode selection that
    is used to calculate the center frequecy on which SAP operates.
    
    Change-Id: Ie110d93cae48d66f257f5a6ec0e17773679ab354
    CRs-Fixed: 726115

--- CORE/HDD/src/wlan_hdd_assoc.c
+++ CORE/HDD/src/wlan_hdd_assoc.c
@@ -1971,7 +1971,8 @@ static eHalStatus hdd_AssociationCompletionHandler( hdd_adapter_t *pAdapter, tCs
         }
         sme_SelectCBMode(WLAN_HDD_GET_HAL_CTX(sap_adapter),
         sapConvertSapPhyModeToCsrPhyMode(hdd_ap_ctx->sapConfig.SapHw_mode),
-                                         hdd_ap_ctx->sapConfig.channel);
+                                         hdd_ap_ctx->sapConfig.channel,
+                                         pHddCtx->cfg_ini->vhtChannelWidth);
         /*
          * Create a workqueue and let the workqueue handle the restarting
          * sap task. if we directly call sap restart function without
