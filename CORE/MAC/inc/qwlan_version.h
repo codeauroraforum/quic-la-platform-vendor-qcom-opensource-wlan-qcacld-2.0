80c91c2c460f763716fa16d70a3fa8c97ec90685
Nandini Suresh <snandini@codeaurora.org>
2014-11-15 14:15:56 -0800

    Release 4.0.2.20LE MDM9X35LE
    Release 4.0.2.20LE MDM9X35LE
    
    Change-Id: Ied753d285502d14acda27acaea6ca67327cc46a8

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -40,11 +40,11 @@ BRIEF DESCRIPTION:
 
 #define QWLAN_VERSION_MAJOR            4
 #define QWLAN_VERSION_MINOR            0
-#define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            236
+#define QWLAN_VERSION_PATCH            2
+#define QWLAN_VERSION_EXTRA            "LE"
+#define QWLAN_VERSION_BUILD            20
 
-#define QWLAN_VERSIONSTR               "1.0.0.236"
+#define QWLAN_VERSIONSTR               "4.0.2.20LE"
 
 #ifdef QCA_WIFI_2_0
 
