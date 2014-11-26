1947f6f8718321e0d7165c9f7e5262971ef3c09f
Wilson Yang <wilson_yang@codeaurora.org>
2014-11-26 07:57:04 -0800

    SDIO Release 04.02.02.006
    SDIO Release 04.02.02.006
    
    Change-Id: Ida8160847bdc959f49ad8958da890958a4ba73e3
    CRs-fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -43,9 +43,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            02
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            005
+#define QWLAN_VERSION_BUILD            006
 
-#define QWLAN_VERSIONSTR               "04.02.02.005"
+#define QWLAN_VERSIONSTR               "04.02.02.006"
 
 
 #define AR6320_REV1_VERSION             0x5000000
