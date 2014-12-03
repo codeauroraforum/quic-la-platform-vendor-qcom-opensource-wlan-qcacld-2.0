23e454ecbfc24fe69d7675464376c4a65ee2cb70
Varun Reddy Yeturu <vyeturu@qca.qualcomm.com>
2014-12-03 11:18:31 -0800

    wlan: qca_cld: Incorrect vdev_id passed during AddTsReq
    The vdev id that is being passed down to the firmware
    as part of the WDA_ADD_TS_REQ is incorrect.
    
    Change-Id: Ide7dcc2d491ad4cbbfbec3ec58266c8aae9a3fb3
    CRs-Fixed: 765999

--- CORE/WDA/inc/legacy/halMsgApi.h
+++ CORE/WDA/inc/legacy/halMsgApi.h
@@ -1070,6 +1070,7 @@ typedef struct
 #ifdef WLAN_FEATURE_ROAM_OFFLOAD
   tANI_U8          setRICparams;
 #endif
+  uint8_t          sme_session_id;
 } tAddTsParams, *tpAddTsParams;
 
 typedef struct
