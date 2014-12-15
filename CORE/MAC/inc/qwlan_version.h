d7bf70811d6fe64aea3130fe11155c19284908e8
Nandini Suresh <snandini@codeaurora.org>
2014-12-15 10:19:37 -0800

    cafstaging release 1.0.0.255B
    cafstaging release 1.0.0.255B
    
    Change-Id: I1276b2cd7f3583df849bc6808253261c11540f68

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -38,13 +38,13 @@ BRIEF DESCRIPTION:
 
 ===========================================================================*/
 
-#define QWLAN_VERSION_MAJOR            4
+#define QWLAN_VERSION_MAJOR            1
 #define QWLAN_VERSION_MINOR            0
-#define QWLAN_VERSION_PATCH            2
-#define QWLAN_VERSION_EXTRA            "LE"
-#define QWLAN_VERSION_BUILD            29
+#define QWLAN_VERSION_PATCH            0
+#define QWLAN_VERSION_EXTRA            "B"
+#define QWLAN_VERSION_BUILD            255
 
-#define QWLAN_VERSIONSTR               "4.0.2.29LE"
+#define QWLAN_VERSIONSTR               "1.0.0.255B"
 
 #ifdef QCA_WIFI_2_0
 
