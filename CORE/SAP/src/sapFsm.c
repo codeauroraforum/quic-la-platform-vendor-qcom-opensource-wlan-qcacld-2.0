92450f3ddfd370b2ea41ba02268b942fea80e7f5
Chandrasekaran, Manishekar <cmshekar@qti.qualcomm.com>
2015-01-13 22:51:53 +0530

    qcacld: Changes to support P2P-GO in DFS Master mode
    This change provides support for DFS support for
    P2P GO mode. No STA connection will be allowed even
    when one beaconing entity is present on DFS channel
    and new vendor commands are used to indicate the
    radar events to userspace using the existing NL
    attributes.
    
    Change-Id: Icf450ddb66523a519ddb854d0a34d82075f9070d
    CRs-Fixed: 780154

--- CORE/SAP/src/sapFsm.c
+++ CORE/SAP/src/sapFsm.c
@@ -2051,7 +2051,7 @@ sapGotoChannelSel
     }
 
 #ifdef WLAN_FEATURE_MBSSID
-    if (vos_concurrent_sap_sessions_running()) {
+    if (vos_concurrent_beaconing_sessions_running()) {
         v_U16_t con_sap_ch = sme_GetConcurrentOperationChannel(hHal);
 
         if (con_sap_ch && sapContext->channel == AUTO_CHANNEL_SELECT) {
@@ -2847,7 +2847,8 @@ ptSapContext sap_find_valid_concurrent_session (tHalHandle hHal)
 
     for (intf = 0; intf < SAP_MAX_NUM_SESSION; intf++)
     {
-         if (VOS_STA_SAP_MODE == pMac->sap.sapCtxList [intf].sapPersona &&
+         if (((VOS_STA_SAP_MODE == pMac->sap.sapCtxList [intf].sapPersona) ||
+             (VOS_P2P_GO_MODE == pMac->sap.sapCtxList [intf].sapPersona)) &&
              pMac->sap.sapCtxList[intf].pSapContext != NULL)
          {
              return pMac->sap.sapCtxList[intf].pSapContext;
@@ -2948,7 +2949,8 @@ void sap_CacResetNotify(tHalHandle hHal)
     {
          ptSapContext pSapContext =
                     (ptSapContext)pMac->sap.sapCtxList [intf].pSapContext;
-         if (VOS_STA_SAP_MODE == pMac->sap.sapCtxList [intf].sapPersona &&
+         if (((VOS_STA_SAP_MODE == pMac->sap.sapCtxList [intf].sapPersona) ||
+             (VOS_P2P_GO_MODE == pMac->sap.sapCtxList [intf].sapPersona)) &&
              pMac->sap.sapCtxList [intf].pSapContext != NULL)
          {
               pSapContext->isCacStartNotified = VOS_FALSE;
@@ -2980,7 +2982,8 @@ VOS_STATUS sap_CacStartNotify(tHalHandle hHal)
     {
          ptSapContext pSapContext =
                     (ptSapContext)pMac->sap.sapCtxList [intf].pSapContext;
-         if (VOS_STA_SAP_MODE == pMac->sap.sapCtxList [intf].sapPersona &&
+         if (((VOS_STA_SAP_MODE == pMac->sap.sapCtxList [intf].sapPersona) ||
+             (VOS_P2P_GO_MODE == pMac->sap.sapCtxList [intf].sapPersona)) &&
              pMac->sap.sapCtxList [intf].pSapContext != NULL &&
              (VOS_FALSE == pSapContext->isCacStartNotified))
          {
@@ -3033,7 +3036,8 @@ VOS_STATUS sap_CacEndNotify(tHalHandle hHal, tCsrRoamInfo *roamInfo)
      {
            ptSapContext pSapContext =
                (ptSapContext)pMac->sap.sapCtxList [intf].pSapContext;
-           if (VOS_STA_SAP_MODE == pMac->sap.sapCtxList [intf].sapPersona &&
+           if (((VOS_STA_SAP_MODE == pMac->sap.sapCtxList [intf].sapPersona) ||
+               (VOS_P2P_GO_MODE == pMac->sap.sapCtxList [intf].sapPersona)) &&
                pMac->sap.sapCtxList [intf].pSapContext != NULL &&
                (VOS_FALSE == pSapContext->isCacEndNotified))
            {
@@ -3362,7 +3366,7 @@ sapFsm
                   * ACS check if AP1 ACS resulting channel is DFS and if yes
                   * override AP2 ACS scan result with AP1 DFS channel
                   */
-                 if (vos_concurrent_sap_sessions_running()) {
+                 if (vos_concurrent_beaconing_sessions_running()) {
                      v_U16_t con_ch;
 
                      con_ch = sme_GetConcurrentOperationChannel(hHal);
@@ -3451,8 +3455,10 @@ sapFsm
                 for (intf = 0; intf < SAP_MAX_NUM_SESSION; intf++)
                 {
                      ptSapContext sapContext;
-                     if (VOS_STA_SAP_MODE ==
-                           pMac->sap.sapCtxList[intf].sapPersona &&
+                     if (((VOS_STA_SAP_MODE ==
+                           pMac->sap.sapCtxList[intf].sapPersona) ||
+                          (VOS_P2P_GO_MODE ==
+                           pMac->sap.sapCtxList[intf].sapPersona)) &&
                            pMac->sap.sapCtxList [intf].pSapContext != NULL)
                      {
                          sapContext = pMac->sap.sapCtxList [intf].pSapContext;
@@ -3648,7 +3654,10 @@ sapFsm
                     {
                          ptSapContext pSapContext;
 
-                        if (VOS_STA_SAP_MODE == pMac->sap.sapCtxList [intf].sapPersona &&
+                        if (((VOS_STA_SAP_MODE ==
+                                pMac->sap.sapCtxList [intf].sapPersona) ||
+                             (VOS_P2P_GO_MODE ==
+                                pMac->sap.sapCtxList [intf].sapPersona)) &&
                           pMac->sap.sapCtxList [intf].pSapContext != NULL )
                         {
                             pSapContext = pMac->sap.sapCtxList [intf].pSapContext;
@@ -4659,7 +4668,8 @@ v_U8_t sap_get_total_number_sap_intf(tHalHandle hHal)
     v_U8_t intf_count = 0;
 
     for (intf = 0; intf < SAP_MAX_NUM_SESSION; intf++) {
-         if (VOS_STA_SAP_MODE == pMac->sap.sapCtxList [intf].sapPersona &&
+         if (((VOS_STA_SAP_MODE == pMac->sap.sapCtxList [intf].sapPersona) ||
+             (VOS_P2P_GO_MODE == pMac->sap.sapCtxList [intf].sapPersona)) &&
              pMac->sap.sapCtxList[intf].pSapContext != NULL) {
              intf_count++;
          }
@@ -4679,7 +4689,8 @@ tANI_BOOLEAN is_concurrent_sap_ready_for_channel_change(tHalHandle hHal,
     v_U8_t intf = 0;
 
     for (intf = 0; intf < SAP_MAX_NUM_SESSION; intf++) {
-         if (VOS_STA_SAP_MODE == pMac->sap.sapCtxList [intf].sapPersona &&
+         if (((VOS_STA_SAP_MODE == pMac->sap.sapCtxList [intf].sapPersona) ||
+             (VOS_P2P_GO_MODE == pMac->sap.sapCtxList [intf].sapPersona)) &&
              pMac->sap.sapCtxList[intf].pSapContext != NULL) {
              pSapContext =
                     (ptSapContext)pMac->sap.sapCtxList [intf].pSapContext;
