50aca062944e6ecd9b57b55ff56749060d1e3564
Satyanarayana Dash <sadash@codeaurora.org>
2015-02-19 12:12:16 +0530

    SDIO Release 4.2.73.26
    SDIO Release 4.2.73.26
    
    Change-Id: Ib7549ea40f5ea175e04ccc966d827fdfb74ebb3b
    CRs-fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -43,9 +43,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            73
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            25
+#define QWLAN_VERSION_BUILD            26
 
-#define QWLAN_VERSIONSTR               "4.2.73.25"
+#define QWLAN_VERSIONSTR               "4.2.73.26"
 
 
 #define AR6320_REV1_VERSION             0x5000000
