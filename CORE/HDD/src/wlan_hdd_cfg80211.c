38535c6ee38098aa183cffdb1802b7ce72793b68
Manikandan Mohan <manikand@qca.qualcomm.com>
2015-02-18 21:46:19 -0800

    qcacld: HDD:Fix AP-AP ACS optimization hostapd acs
    Fixed AP-AP acs optimization issues after pre start bss acs
    driven from hostapd.
    
    Change-Id: I6c143faa2d94ff38c02899e3c7deda91c7414e62
    CRs-fixed: 792523

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -999,7 +999,7 @@ static int wlan_hdd_config_acs(hdd_context_t *hdd_ctx, hdd_adapter_t *adapter)
 
         sap_config->skip_acs_scan_status = eSAP_DO_NEW_ACS_SCAN;
 
-        if (con_sap_config && con_sap_config->channel == AUTO_CHANNEL_SELECT &&
+        if (con_sap_config && con_sap_config->acs_case == true &&
             hdd_ctx->skip_acs_scan_status == eSAP_SKIP_ACS_SCAN) {
 
             hddLog(LOG1, FL("Operating Band: PriAP: %d SecAP: %d"),
@@ -2935,11 +2935,6 @@ static int wlan_hdd_cfg80211_start_bss(hdd_adapter_t *pHostapdAdapter,
 
     hddLog(VOS_TRACE_LEVEL_INFO_HIGH,"****pConfig->dtim_period=%d***",
                                       pConfig->dtim_period);
-    status = wlan_hdd_config_acs(pHddCtx, pHostapdAdapter);
-    if (status) {
-        hddLog(LOGE, FL("ACS config failed"));
-        return -EINVAL;
-    }
 
     if (pHostapdAdapter->device_mode == WLAN_HDD_SOFTAP)
     {
@@ -3033,6 +3028,12 @@ static int wlan_hdd_cfg80211_start_bss(hdd_adapter_t *pHostapdAdapter,
         }
         else
         {
+            status = wlan_hdd_config_acs(pHddCtx, pHostapdAdapter);
+            if (status) {
+                hddLog(LOGE, FL("ACS config failed"));
+                return -EINVAL;
+            }
+
              if(1 != pHddCtx->is_dynamic_channel_range_set)
              {
 #ifdef WLAN_FEATURE_MBSSID
