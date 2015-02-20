bc5bf4763d009272d891ecee8e28a8dda8568f6e
Nandini Suresh <snandini@qca.qualcomm.com>
2015-02-20 12:12:14 -0800

    Release 4.4.10.001 MDM9X40.
    Release 4.4.10.001 MDM9X40.
    
    Change-Id: I25a62e7274783fff4505d245e82038321502671b
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -39,12 +39,12 @@ BRIEF DESCRIPTION:
 ===========================================================================*/
 
 #define QWLAN_VERSION_MAJOR            4
-#define QWLAN_VERSION_MINOR            0
+#define QWLAN_VERSION_MINOR            4
 #define QWLAN_VERSION_PATCH            10
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            39 
+#define QWLAN_VERSION_BUILD            001 
 
-#define QWLAN_VERSIONSTR               "4.0.10.39"
+#define QWLAN_VERSIONSTR               "4.4.10.001"
 
 
 #define AR6320_REV1_VERSION             0x5000000
