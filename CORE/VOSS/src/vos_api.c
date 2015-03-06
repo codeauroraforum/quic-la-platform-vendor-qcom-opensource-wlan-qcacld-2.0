c5f25b5e78c131522083212a8775f075be8b8a64
Edhar, Mahesh Kumar <c_medhar@qti.qualcomm.com>
2015-03-06 15:23:18 +0530

    HDD: Allocate/free dumpTableEntry memory as part of mac module open/close
    Currently dumpTableEntry is allocated as part of macPreStart but freed
    during macStop function due to which in failure cases we are calling
    macstop even though macStart has not called.
    Changes are made to fix the above error handling scenario by allocate/free
    the dumpTableEntry memory as part of mac open/close functions.
    
    Change-Id: I69357719686b871222be852ab2a3765401376316
    CRs-Fixed: 803710
    Git-commit: c2b3d3b8819825f9ea926296c7e11ec5fca5e3c2

--- CORE/VOSS/src/vos_api.c
+++ CORE/VOSS/src/vos_api.c
@@ -704,7 +704,6 @@ VOS_STATUS vos_preStart( v_CONTEXT_t vosContext )
    {
       VOS_TRACE(VOS_MODULE_ID_SYS, VOS_TRACE_LEVEL_FATAL,
              "Failed to WDA prestart");
-      macStop(gpVosContext->pMACContext, HAL_STOP_TYPE_SYS_DEEP_SLEEP);
       ccmStop(gpVosContext->pMACContext);
       VOS_ASSERT(0);
       return VOS_STATUS_E_FAILURE;
@@ -729,7 +728,6 @@ VOS_STATUS vos_preStart( v_CONTEXT_t vosContext )
            "%s: Test MC thread by posting a probe message to SYS", __func__);
       wlan_sys_probe();
 
-      macStop(gpVosContext->pMACContext, HAL_STOP_TYPE_SYS_DEEP_SLEEP);
       ccmStop(gpVosContext->pMACContext);
       VOS_ASSERT( 0 );
       return VOS_STATUS_E_FAILURE;
@@ -740,7 +738,6 @@ VOS_STATUS vos_preStart( v_CONTEXT_t vosContext )
    {
       VOS_TRACE(VOS_MODULE_ID_SYS, VOS_TRACE_LEVEL_FATAL,
                "Failed to Start HTC");
-      macStop(gpVosContext->pMACContext, HAL_STOP_TYPE_SYS_DEEP_SLEEP);
       ccmStop(gpVosContext->pMACContext);
       VOS_ASSERT( 0 );
       return VOS_STATUS_E_FAILURE;
@@ -751,7 +748,6 @@ VOS_STATUS vos_preStart( v_CONTEXT_t vosContext )
       VOS_TRACE(VOS_MODULE_ID_SYS, VOS_TRACE_LEVEL_FATAL,
                "Failed to get ready event from target firmware");
       HTCSetTargetToSleep(scn);
-      macStop(gpVosContext->pMACContext, HAL_STOP_TYPE_SYS_DEEP_SLEEP);
       ccmStop(gpVosContext->pMACContext);
       HTCStop(gpVosContext->htc_ctx);
       VOS_ASSERT( 0 );
