44de02dcbe5542e84a2e517da60f55c4195b16a8
Venkata Rajesh Kumar Pitani <vpitan@codeaurora.org>
2015-04-02 11:31:54 +0530

    Cafstaging Release 4.0.10.66
    Cafstaging Release 4.0.10.66
    
    Change-Id: I4582fff01c1f2acb14373a121024a21306bc8070
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -39,12 +39,12 @@ BRIEF DESCRIPTION:
 ===========================================================================*/
 
 #define QWLAN_VERSION_MAJOR            4
-#define QWLAN_VERSION_MINOR            4
+#define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            10
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            8
+#define QWLAN_VERSION_BUILD            66
 
-#define QWLAN_VERSIONSTR               "4.4.10.8"
+#define QWLAN_VERSIONSTR               "4.0.10.66"
 
 
 #define AR6320_REV1_VERSION             0x5000000
