f8b058385772aff15f82677840e74f4bb3032c8c
Jeff Johnson <jjohnson@qca.qualcomm.com>
2015-04-02 13:27:52 -0700

    qcacld-2.0: Synchronize BSSID hotlist operations
    Currently the result of the Set BSSID Hotlist and Reset BSSID Hotlist
    operations are sent asynchronously to Wi-Fi HAL.  However the Wi-Fi
    HAL expects these operations to be synchronous.  Update the
    implementation such that the result of the operation is returned as
    the result of the vendor command.
    
    Change-Id: I4dda5370cac93e703ff00bd8ef872f4edf8cda53
    CRs-Fixed: 817392

--- CORE/HDD/inc/wlan_hdd_main.h
+++ CORE/HDD/inc/wlan_hdd_main.h
@@ -108,6 +108,7 @@
    a response from WCNSS */
 #define WLAN_WAIT_TIME_SESSIONOPENCLOSE  15000
 #define WLAN_WAIT_TIME_ABORTSCAN  2000
+#define WLAN_WAIT_TIME_EXTSCAN  1000
 
 
 /** Maximum time(ms) to wait for mc thread suspend **/
@@ -1209,6 +1210,21 @@ typedef struct
 
 }fw_log_info;
 
+#ifdef FEATURE_WLAN_EXTSCAN
+/**
+ * struct hdd_ext_scan_context - hdd ext scan context
+ * @request_id: userspace-assigned ID associated with the request
+ * @response_event: Ext scan wait event
+ * @response_status: Status returned by FW in response to a request
+ *
+ */
+struct hdd_ext_scan_context {
+	uint32_t request_id;
+	int response_status;
+	struct completion response_event;
+};
+#endif
+
 /** Adapter stucture definition */
 
 struct hdd_context_s
@@ -1492,6 +1508,10 @@ struct hdd_context_s
 
     /* Time since boot up to WiFi turn ON (in micro seconds) */
     v_U64_t wifi_turn_on_time_since_boot;
+
+#ifdef FEATURE_WLAN_EXTSCAN
+    struct hdd_ext_scan_context ext_scan_context;
+#endif /* FEATURE_WLAN_EXTSCAN */
 };
 
 /*---------------------------------------------------------------------------
