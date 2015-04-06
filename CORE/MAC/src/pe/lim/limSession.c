14c6e9619dee89ca0521a56e35895f6d421e0115
Karthick S <skarthic@qti.qualcomm.com>
2015-04-06 11:53:46 +0530

    qcacld-2.0: Free protection_fields_reset_timer
    Free protection_fields_reset_timer in the beginning of peDeleteSession
    function to avoid the null defreference caused due to the race condition
    between peDeleteSession and pe_reset_protection_callback.
    
    Change-Id: I1b83977bb3683d6dd87ec94a992ac51f39cd97db
    CRs-Fixed: 815796
    Git-commit: 634f9b0a64b5c0a37df340a8564995baf7d7b98b

--- CORE/MAC/src/pe/lim/limSession.c
+++ CORE/MAC/src/pe/lim/limSession.c
@@ -552,6 +552,11 @@ void peDeleteSession(tpAniSirGlobal pMac, tpPESession psessionEntry)
         }
     }
 
+    if (LIM_IS_AP_ROLE(psessionEntry)) {
+       vos_timer_stop(&psessionEntry->protection_fields_reset_timer);
+       vos_timer_destroy(&psessionEntry->protection_fields_reset_timer);
+    }
+
 #if defined (WLAN_FEATURE_VOWIFI_11R)
     /* Delete FT related information */
     limFTCleanup(pMac, psessionEntry);
@@ -720,11 +725,6 @@ void peDeleteSession(tpAniSirGlobal pMac, tpPESession psessionEntry)
         vos_timer_destroy(&psessionEntry->pmfComebackTimer);
     }
 #endif
-
-    if (LIM_IS_AP_ROLE(psessionEntry)) {
-       vos_timer_stop(&psessionEntry->protection_fields_reset_timer);
-       vos_timer_destroy(&psessionEntry->protection_fields_reset_timer);
-    }
 
     psessionEntry->valid = FALSE;
     return;
