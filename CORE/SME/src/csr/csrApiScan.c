86b06101e388ab170bc20920a1675774e162ccb3
Varun Reddy Yeturu <vyeturu@qca.qualcomm.com>
2015-04-27 17:59:23 -0700

    qcacld-2.0: Initial Scan fix
    CSR module assumes that the first scan generated
    will have a scan id equal to Zero, but
    the scan id is being initialized to 1 and hence
    CSR would never interpret the first scan properly.
    
    CRs-Fixed: 829292
    Change-Id: I7f113748631710293180ab637127b19a62df12fb

--- CORE/SME/src/csr/csrApiScan.c
+++ CORE/SME/src/csr/csrApiScan.c
@@ -554,11 +554,12 @@ static void csrScan2GOnyRequest(tpAniSirGlobal pMac,tSmeCmd *pScanCmd,
         return;
     }
 
-    if (pScanCmd->u.scanCmd.scanID ||
+    if ((pScanCmd->u.scanCmd.scanID != FIRST_SCAN_ID) ||
        (eCSR_SCAN_REQUEST_FULL_SCAN != pScanRequest->requestType))
            return;
 
-    //Contsruct valid Supported 2.4 GHz Channel List
+    smsLog( pMac, LOG1, FL("Scanning only 2G Channels during first scan"));
+    /* Construct valid Supported 2.4 GHz Channel List */
     for( index = 0; index < ARRAY_SIZE(channelList2G); index++ )
     {
         channelId = channelList2G[index];
@@ -724,7 +725,7 @@ eHalStatus csrScanRequest(tpAniSirGlobal pMac, tANI_U16 sessionId,
                 // If it is the first scan request from HDD, CSR checks if it is for 11d.
                 // If it is not, CSR will save the scan request in the pending cmd queue
                 // & issue an 11d scan request to PE.
-                if (((0 == pScanCmd->u.scanCmd.scanID)
+                if (((FIRST_SCAN_ID == pScanCmd->u.scanCmd.scanID)
                    && (eCSR_SCAN_REQUEST_11D_SCAN != pScanRequest->requestType))
 #ifdef SOFTAP_CHANNEL_RANGE
                    && (eCSR_SCAN_SOFTAP_CHANNEL_RANGE != pScanRequest->requestType)
@@ -844,7 +845,6 @@ eHalStatus csrScanRequest(tpAniSirGlobal pMac, tANI_U16 sessionId,
                 //Once we turn on Wifi
                 if(pMac->scan.fFirstScanOnly2GChnl)
                 {
-                    smsLog( pMac, LOG1, FL("Scanning only 2G Channels during first scan"));
                     csrScan2GOnyRequest(pMac, pScanCmd, pScanRequest);
                 }
 
