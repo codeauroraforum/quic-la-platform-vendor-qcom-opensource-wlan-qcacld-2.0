16399ab79df1af90b1e3b99cdbcab58ef5b63cb3
Akash Patel <akashp@codeaurora.org>
2015-05-04 19:19:39 -0700

    Release 4.4.21.29
    Release 4.4.21.29
    
    Change-Id: I1e63db6c2adc27ce2c6ed60bd4a2e6345953676b
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            4
 #define QWLAN_VERSION_PATCH            21
 #define QWLAN_VERSION_EXTRA           ""
-#define QWLAN_VERSION_BUILD            28
+#define QWLAN_VERSION_BUILD            29
 
-#define QWLAN_VERSIONSTR               "4.4.21.28"
+#define QWLAN_VERSIONSTR               "4.4.21.29"
 
 
 #define AR6320_REV1_VERSION             0x5000000
