f6535e0a424bd4ce29d722b2a101d3552daf0ac2
Hema Aparna Medicharla <haparna@qti.qualcomm.com>
2015-05-06 13:47:45 -0700

    qcacld-2.0: Release locks before error logging
    This is a prima to qcacld-2.0 propagation.
    
    Changes made are
      1 - to release locks before error logging
    In addition to the prima propagation fixes, this gerrit has few more
    occurences of similar fixes in other files found in CLD code.
    
    CRs-Fixed: 807607
    Change-Id: I01a7656e6a355ce97b74f3a25d969b5283cf1bbb

--- CORE/HDD/src/wlan_hdd_tdls.c
+++ CORE/HDD/src/wlan_hdd_tdls.c
@@ -542,10 +542,10 @@ int wlan_hdd_tdls_init(hdd_adapter_t *pAdapter)
     {
         pHddCtx->tdls_mode = eTDLS_SUPPORT_NOT_ENABLED;
         pAdapter->sessionCtx.station.pHddTdlsCtx = NULL;
+        mutex_unlock(&pHddCtx->tdls_lock);
         hddLog(VOS_TRACE_LEVEL_ERROR,
                "%s TDLS not enabled (%d) or FW doesn't support",
                __func__, pHddCtx->cfg_ini->fEnableTDLSSupport);
-        mutex_unlock(&pHddCtx->tdls_lock);
         return 0;
     }
     /* TDLS is supported only in STA / P2P Client modes,
@@ -574,9 +574,9 @@ int wlan_hdd_tdls_init(hdd_adapter_t *pAdapter)
         pHddTdlsCtx = vos_mem_malloc(sizeof(tdlsCtx_t));
 
         if (NULL == pHddTdlsCtx) {
-            hddLog(VOS_TRACE_LEVEL_ERROR, "%s malloc failed!", __func__);
             pAdapter->sessionCtx.station.pHddTdlsCtx = NULL;
             mutex_unlock(&pHddCtx->tdls_lock);
+            hddLog(VOS_TRACE_LEVEL_ERROR, "%s malloc failed!", __func__);
             return -1;
         }
         /* initialize TDLS pAdater context */
@@ -979,12 +979,10 @@ hddTdlsPeer_t *wlan_hdd_tdls_get_peer(hdd_adapter_t *pAdapter, u8 *mac)
 
     pHddTdlsCtx = WLAN_HDD_GET_TDLS_CTX_PTR(pAdapter);
 
-    if (NULL == pHddTdlsCtx)
-    {
-        VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
-                  FL("pHddTdlsCtx is NULL"));
+    if (NULL == pHddTdlsCtx) {
         vos_mem_free(peer);
         mutex_unlock(&pHddCtx->tdls_lock);
+        hddLog(LOG1, FL("pHddTdlsCtx is NULL"));
         return NULL;
     }
 
@@ -2114,9 +2112,9 @@ void wlan_hdd_tdls_disconnection_callback(hdd_adapter_t *pAdapter)
 
     if (NULL == pHddTdlsCtx)
     {
+       mutex_unlock(&pHddCtx->tdls_lock);
        VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
                 FL("pHddTdlsCtx is NULL"));
-        mutex_unlock(&pHddCtx->tdls_lock);
         return;
     }
     pHddTdlsCtx->discovery_sent_cnt = 0;
