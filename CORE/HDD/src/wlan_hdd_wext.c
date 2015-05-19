44fc4fe49c444dea2198868b45f75b0e6d703d98
Srinivas Girigowda <sgirigow@qca.qualcomm.com>
2015-05-19 11:04:00 -0700

    qcacld-2.0: Remove rtnl_is_locked() check before acquiring lock
    rtnl_is_locked() does not have expected semantics where it should
    return true, if lock is already acquired in current
    context (context from which API is called).
    
    The API is returning true even if the lock is acquired by
    another context (user space in this case) and this is defeating
    the whole purpose of synchronization.
    
    Fix is to remove rtnl_is_locked() check before acquiring lock.
    
    Change-Id: I1a5f0a8539ca72af32c37e9ebd3909eb11448047
    CRs-Fixed: 840371

--- CORE/HDD/src/wlan_hdd_wext.c
+++ CORE/HDD/src/wlan_hdd_wext.c
@@ -11095,33 +11095,12 @@ int hdd_register_wext(struct net_device *dev)
 
 int hdd_UnregisterWext(struct net_device *dev)
 {
-#if 0
-   hdd_wext_state_t *wextBuf;
-   hdd_adapter_t *pAdapter = WLAN_HDD_GET_PRIV_PTR(dev);
+	hddLog(LOG1, FL("dev(%p)"), dev);
 
-   ENTER();
-   // Set up the pointer to the Wireless Extensions state structure
-   wextBuf = pAdapter->pWextState;
-
-   // De-allocate the Wireless Extensions state structure
-   kfree(wextBuf);
-
-   // Clear out the pointer to the Wireless Extensions state structure
-   pAdapter->pWextState = NULL;
-
-   EXIT();
-#endif
-	int islocked = rtnl_is_locked();
-
-	VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO, "In %s", __func__);
-
-	if (!islocked)
+	if (dev != NULL) {
 		rtnl_lock();
-
-	dev->wireless_handlers = NULL;
-
-	if (!islocked)
+		dev->wireless_handlers = NULL;
 		rtnl_unlock();
-
+	}
 	return 0;
 }
