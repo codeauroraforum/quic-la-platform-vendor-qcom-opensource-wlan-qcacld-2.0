6c7fdd7b6c1e68c48d40c790f2edffa9927cc7bf
Madan Mohan Koyyalamudi <mkoyyala@qca.qualcomm.com>
2015-06-09 12:57:34 -0700

    BIT driver 4.1.29.38

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -39,11 +39,11 @@ BRIEF DESCRIPTION:
 ===========================================================================*/
 
 #define QWLAN_VERSION_MAJOR            4
-#define QWLAN_VERSION_MINOR            4
-#define QWLAN_VERSION_PATCH            22
+#define QWLAN_VERSION_MINOR            1
+#define QWLAN_VERSION_PATCH            29
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            26
-#define QWLAN_VERSIONSTR               "4.4.22.026"
+#define QWLAN_VERSION_BUILD            38
+#define QWLAN_VERSIONSTR               "4.1.29.38"
 
 
 #define AR6320_REV1_VERSION             0x5000000
