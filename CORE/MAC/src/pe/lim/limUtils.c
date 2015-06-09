0cf5565b8b6e8bc03f629b559b67c5ea85604bb9
Kanchanapally, Vidyullatha <vkanchan@qti.qualcomm.com>
2015-06-09 11:11:46 +0530

    Revert "qcacld: Send DELBA frame with proper reason code."
    This reverts Change-Id: I5a471cbb5a82a42b80d10a22828746b2902ddf2c.
    
    Block Ack is not handled in host driver in qcacld, hence the
    changes in Change-Id: I5a471cbb5a82a42b80d10a22828746b2902ddf2c
    are not needed.
    
    Change-Id: I7d1dfca2fd5c77f2ef6aff27e9152414217f43d9
    CRs-Fixed: 856607
    Git-commit: 2444b24a9c9f24ca8ab43c298e76fa83a7834466

--- CORE/MAC/src/pe/lim/limUtils.c
+++ CORE/MAC/src/pe/lim/limUtils.c
@@ -5712,7 +5712,7 @@ limProcessAddBaInd(tpAniSirGlobal pMac, tpSirMsgQ limMsg)
 
 void
 limDeleteBASessions(tpAniSirGlobal pMac, tpPESession pSessionEntry,
-                    tANI_U32 baDirection, tSirMacReasonCodes baReasonCode)
+                    tANI_U32 baDirection)
 {
     tANI_U32 i;
     tANI_U8 tid;
@@ -5740,14 +5740,14 @@ limDeleteBASessions(tpAniSirGlobal pMac, tpPESession pSessionEntry,
                                        (baDirection & BA_INITIATOR))
                         {
                             limPostMlmDelBAReq(pMac, pSta, eBA_INITIATOR, tid,
-                                               baReasonCode,
+                                               eSIR_MAC_UNSPEC_FAILURE_REASON,
                                                pSessionEntry);
                         }
                         if ((eBA_ENABLE == pSta->tcCfg[tid].fUseBARx) &&
                                         (baDirection & BA_RECIPIENT))
                         {
                             limPostMlmDelBAReq(pMac, pSta, eBA_RECIPIENT, tid,
-                                               baReasonCode,
+                                               eSIR_MAC_UNSPEC_FAILURE_REASON,
                                                pSessionEntry);
                         }
                     }
@@ -5765,14 +5765,14 @@ limDeleteBASessions(tpAniSirGlobal pMac, tpPESession pSessionEntry,
                                     (baDirection & BA_INITIATOR))
                     {
                         limPostMlmDelBAReq(pMac, pSta, eBA_INITIATOR, tid,
-                                           baReasonCode,
+                                           eSIR_MAC_UNSPEC_FAILURE_REASON,
                                            pSessionEntry);
                     }
                     if ((eBA_ENABLE == pSta->tcCfg[tid].fUseBARx) &&
                                     (baDirection & BA_RECIPIENT))
                     {
                         limPostMlmDelBAReq(pMac, pSta, eBA_RECIPIENT, tid,
-                                           baReasonCode,
+                                           eSIR_MAC_UNSPEC_FAILURE_REASON,
                                            pSessionEntry);
                     }
                 }
@@ -5798,8 +5798,7 @@ void limDelAllBASessions(tpAniSirGlobal pMac)
         pSessionEntry = peFindSessionBySessionId(pMac, i);
         if (pSessionEntry)
         {
-            limDeleteBASessions(pMac, pSessionEntry, BA_BOTH_DIRECTIONS,
-                                eSIR_MAC_UNSPEC_FAILURE_REASON);
+            limDeleteBASessions(pMac, pSessionEntry, BA_BOTH_DIRECTIONS);
         }
     }
 }
@@ -5822,8 +5821,7 @@ void limDelPerBssBASessionsBtc(tpAniSirGlobal pMac)
     {
         PELOGW(limLog(pMac, LOGW,
         "Deleting the BA for session %d as host got BTC event", sessionId);)
-        limDeleteBASessions(pMac, pSessionEntry, BA_RECIPIENT,
-                            eSIR_MAC_PEER_TIMEDOUT_REASON);
+        limDeleteBASessions(pMac, pSessionEntry, BA_RECIPIENT);
     }
 }
 
