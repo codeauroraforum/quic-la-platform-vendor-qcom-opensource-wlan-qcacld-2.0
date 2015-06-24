87247378cf6e6a587a5b41d94148861a1fb748ae
Ganesh Kondabattini <ganeshk@codeaurora.org>
2015-06-24 20:22:20 +0530

    wlan: Avoid race condition in SAP mode
    There is race condition seen when upper layer sends the deauth
    to the STA and at same time STA sends the assocReq frame to SAP.
    In this case LIM sends the Assoc indication to SME and wait for
    the Assoc cnf. Now SME passes the deauth request to Lim and LIM
    deletes this pStaD. When SME sends the assoc Cnf it checks pStaD
    as NULL. So LIM and SME are not in sync here.Fix that.
    
    Change-Id: Iee844eaec543b0a2f1f7ec9d9ef26f464df5697c
    CRs-Fixed: 814751
    Git-commit: 9a6dc10cb95c6ab770e84f96798ea0e36992086d

--- CORE/MAC/src/pe/lim/limSendSmeRspMessages.c
+++ CORE/MAC/src/pe/lim/limSendSmeRspMessages.c
@@ -1399,7 +1399,8 @@ limSendSmeDisassocNtf(tpAniSirGlobal pMac,
     switch (disassocTrigger)
     {
         case eLIM_PEER_ENTITY_DISASSOC:
-            return;
+            if (reasonCode != eSIR_SME_STA_NOT_ASSOCIATED)
+                return;
 
         case eLIM_HOST_DISASSOC:
             /**
