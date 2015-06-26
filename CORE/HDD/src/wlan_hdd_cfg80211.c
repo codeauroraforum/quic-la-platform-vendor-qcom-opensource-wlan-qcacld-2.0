60817e7b7e571541a5a43c3c6cea506a66af3e6d
Manikandan Mohan <manikand@qca.qualcomm.com>
2015-06-25 18:19:23 -0700

    qcacld: fix HT operation info IE for 20 Mhz
    Fixed the incorrect setting of HT operation info IE in beacon,
    reporting support for 20 and 40 Mhz clients with SAP in 20 Mhz
    operation. OBSS support flag determines this IE element, so
    configured in properly for HT20 Mode.
    
    Change-Id: I59f6eb10d6fbbf981d751253185db142c4ddcda3
    CRs-fixed: 861881
    Git-commit: a4f0dc8a0ab83969772744f8b17ebd902619b4cd

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -6660,6 +6660,7 @@ static int wlan_hdd_cfg80211_set_channel( struct wiphy *wiphy, struct net_device
             switch (channel_type) {
             case NL80211_CHAN_HT20:
             case NL80211_CHAN_NO_HT:
+                smeConfig.csrConfig.obssEnabled = VOS_FALSE;
                 if (channel <= 14)
                     smeConfig.csrConfig.channelBondingMode24GHz =
                                            eCSR_INI_SINGLE_CHANNEL_CENTERED;
