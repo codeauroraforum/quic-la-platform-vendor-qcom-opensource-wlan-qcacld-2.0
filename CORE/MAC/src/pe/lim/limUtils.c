769cb0552f765f15147c8b03ce71a0eff492fa38
Naveen Rawat <nrawat@qca.qualcomm.com>
2015-07-02 10:08:16 -0700

    qcacld-2.0: HT20 Protection Mode
    HT_PURE mode indicates either only HT20 STAs are connected
    to a HT20 only SAP or HT20/40 STAs are connected to HT20/40
    SAP. Otherwise its HT20 protection mode. This patch fixes code
    to work accroding to this condition correctly.
    
    Change-Id: Ibaf502254f505b87c448ea5614e60316ad8c1dd5
    CRs-Fixed: 866447

--- CORE/MAC/src/pe/lim/limUtils.c
+++ CORE/MAC/src/pe/lim/limUtils.c
@@ -3533,7 +3533,13 @@ limEnable11gProtection(tpAniSirGlobal pMac, tANI_U8 enable,
                             limEnableHtRifsProtection(pMac, false, overlap, pBeaconParams,psessionEntry);
                             limEnableHtOBSSProtection(pMac,  false, overlap, pBeaconParams,psessionEntry);
                             if (psessionEntry->gLimHt20Params.protectionEnabled) {
-                                psessionEntry->htOperMode = eSIR_HT_OP_MODE_PURE;
+                                if (eHT_CHANNEL_WIDTH_20MHZ ==
+                                      psessionEntry->htSupportedChannelWidthSet)
+                                    psessionEntry->htOperMode =
+                                        eSIR_HT_OP_MODE_PURE;
+                                else
+                                    psessionEntry->htOperMode =
+                                        eSIR_HT_OP_MODE_NO_LEGACY_20MHZ_HT;
                             } else
                                 psessionEntry->htOperMode = eSIR_HT_OP_MODE_PURE;
                         }
@@ -4081,7 +4087,10 @@ limEnableHT20Protection(tpAniSirGlobal pMac, tANI_U8 enable,
                psessionEntry->gLimHt20Params.protectionEnabled = true;
                 if(eSIR_HT_OP_MODE_PURE == psessionEntry->htOperMode)
                 {
-                    psessionEntry->htOperMode = eSIR_HT_OP_MODE_NO_LEGACY_20MHZ_HT;
+                    if (psessionEntry->htSupportedChannelWidthSet !=
+                            eHT_CHANNEL_WIDTH_20MHZ)
+                        psessionEntry->htOperMode =
+                            eSIR_HT_OP_MODE_NO_LEGACY_20MHZ_HT;
                     limEnableHtRifsProtection(pMac, false, overlap, pBeaconParams,psessionEntry);
                     limEnableHtOBSSProtection(pMac,  false, overlap, pBeaconParams,psessionEntry);
                 }
@@ -4140,7 +4149,11 @@ limEnableHT20Protection(tpAniSirGlobal pMac, tANI_U8 enable,
                     {
                         if(psessionEntry->gLimHt20Params.protectionEnabled)
                         {
-                            psessionEntry->htOperMode = eSIR_HT_OP_MODE_PURE;
+                            if (psessionEntry->htSupportedChannelWidthSet ==
+                                    eHT_CHANNEL_WIDTH_20MHZ)
+                               psessionEntry->htOperMode = eSIR_HT_OP_MODE_PURE;
+                            else
+                               psessionEntry->htOperMode = eSIR_HT_OP_MODE_NO_LEGACY_20MHZ_HT;
                             limEnableHtRifsProtection(pMac, false, overlap, pBeaconParams,psessionEntry);
                             limEnableHtOBSSProtection(pMac,  false, overlap, pBeaconParams,psessionEntry);
                         }
