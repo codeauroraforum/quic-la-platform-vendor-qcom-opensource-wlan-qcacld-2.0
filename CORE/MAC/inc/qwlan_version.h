bccd9d7df728f4816d053531335fe3232ae7311f
Anjaneedevi Kapparapu <akappa@codeaurora.org>
2015-07-09 15:12:37 +0530

    Release 4.2.1.10
    Release 4.2.1.10
    
    Change-Id: I753464f2fda38106d4f2635c681f85ecb5cb74b7
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -43,9 +43,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH             1
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            9
+#define QWLAN_VERSION_BUILD            10
 
-#define QWLAN_VERSIONSTR               "4.2.1.9"
+#define QWLAN_VERSIONSTR               "4.2.1.10"
 
 
 #define AR6320_REV1_VERSION             0x5000000
