6dc59dac99cf7ad230d67ec5d00b237350a1871d
Edhar, Mahesh Kumar <c_medhar@qti.qualcomm.com>
2015-07-13 11:33:17 +0530

    qcacld-2.0: Change to drop received beacon in deferred state
    Currently while sending command to firmware and waiting for
    response we will set gLimProcessDefdMsgs to false, during
    this processing of lim request message will be deferred until
    we get the response.
    
    In noisy environment we might run out of message wrappers on
    receiving lot of beacon frames in deferred state.
    
    Change to drop received beacon during deferred state.
    
    Change-Id: I709574d3a97f89a21500c3fbbb166bb257d26c7b
    CRs-Fixed: 870159

--- CORE/SYS/legacy/src/system/src/sysEntryFunc.c
+++ CORE/SYS/legacy/src/system/src/sysEntryFunc.c
@@ -162,6 +162,19 @@ sysBbtProcessMessageCore(tpAniSirGlobal pMac, tpSirMsgQ pMsg, tANI_U32 type,
                 goto fail;
             }
 
+            /*
+             * Drop beacon frames in deferred state to avoid VOSS run out of
+             * message wrappers.
+             */
+            if ((subType == SIR_MAC_MGMT_BEACON) &&
+                (!limIsSystemInScanState(pMac)) &&
+                (true != GET_LIM_PROCESS_DEFD_MESGS(pMac)) &&
+                !pMac->lim.gLimSystemInScanLearnMode) {
+                VOS_TRACE(VOS_MODULE_ID_SYS, VOS_TRACE_LEVEL_INFO_HIGH,
+                          FL("dropping received beacon in deffered state"));
+                goto fail;
+            }
+
             if (subType == SIR_MAC_MGMT_DEAUTH)
             {
                 tpSirMacMgmtHdr pMacHdr = WDA_GET_RX_MAC_HEADER(pBd);
