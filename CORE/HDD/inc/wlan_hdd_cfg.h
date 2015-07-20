c67a1b444bc41c00246d603bf4695a012ec8d589
Abhishek Singh <absingh@qti.qualcomm.com>
2015-07-20 12:02:47 +0530

    qcacld-2.0: Add INI param to ignore ERP info in non 11b AP beacon.
    prima to qcacld-2.0 propagation
    
    Add INI param to ignore ERP info in the non 11b AP beacon.
    With this param set the 11b protection will be enabled only if
    driver receive an 11b beacon.
    
    CRs-Fixed: 853250
    Change-Id: I1c7e0e40c609569b322dda3c30fd4a203a2bd987
    Git-commit: 57202da36ee2d113809edb70c4a02611072952b2

--- CORE/HDD/inc/wlan_hdd_cfg.h
+++ CORE/HDD/inc/wlan_hdd_cfg.h
@@ -2609,6 +2609,11 @@ This feature requires the dependent cfg.ini "gRoamPrefer5GHz" set to 1 */
 #define CFG_RA_RATE_LIMIT_INTERVAL_DEFAULT         (60)/*60 SEC*/
 #endif
 
+#define CFG_IGNORE_PEER_ERP_INFO_NAME      "gIgnorePeerErpInfo"
+#define CFG_IGNORE_PEER_ERP_INFO_MIN       ( 0 )
+#define CFG_IGNORE_PEER_ERP_INFO_MAX       ( 1 )
+#define CFG_IGNORE_PEER_ERP_INFO_DEFAULT   ( 0 )
+
 #define CFG_INITIAL_DWELL_TIME_NAME            "gInitialDwellTime"
 #define CFG_INITIAL_DWELL_TIME_DEFAULT         (0)
 #define CFG_INITIAL_DWELL_TIME_MIN             (0)
@@ -3572,6 +3577,7 @@ typedef struct
 #ifdef FEATURE_SECURE_FIRMWARE
    bool                        enable_fw_hash_check;
 #endif
+   v_BOOL_t                    ignorePeerErpInfo;
    uint16_t                    pkt_err_disconn_th;
    bool                        tx_chain_mask_cck;
    uint8_t                     tx_chain_mask_1ss;
