0f17429a17ecf9a8f9b7d1d9af81178107628763
Sushant Kaushik <skaushik@qti.qualcomm.com>
2015-07-22 11:19:02 +0530

    wlan: Limit the Deauth Frames sent by AP to STA
    Currently when Scan is ongoing and Deauth is sent by AP to STA,
    LIM post the DEAUTH IND message to CSR.Because of scan, FW keeps
    on sending the NULL frame and AP sends the DEAUTH frame in
    response to this NULL frame.This results in flood of DEAUTH frame
    and PE message queue always have this DEAUTH frames because of which
    CSR is not able to get chance to process the DEAUTH indication.
    
    As a part of fix deauth frames RX are limited  and for PMF
    unproctected frames only frames with time difference of 1 sec
    are passed to Lim.
    
    Change-Id: I8843f0806938be6194361b4c569845e4a735a76e
    CRs-Fixed: 864583
    Git-commit: 59e12d6aa89f07b09ea298bf5fde40b8e1be2fc5

--- CORE/SYS/legacy/src/system/src/sysEntryFunc.c
+++ CORE/SYS/legacy/src/system/src/sysEntryFunc.c
@@ -114,7 +114,6 @@ tSirRetStatus
 sysBbtProcessMessageCore(tpAniSirGlobal pMac, tpSirMsgQ pMsg, tANI_U32 type,
                          tANI_U32 subType)
 {
-    static tANI_U32 lastDeauthPacketTime = 0;
     tSirRetStatus ret;
     void*         pBd;
     tMgmtFrmDropReason dropReason;
@@ -153,27 +152,11 @@ sysBbtProcessMessageCore(tpAniSirGlobal pMac, tpSirMsgQ pMsg, tANI_U32 type,
                     goto fail;
                 }
             }
-            if ((subType == SIR_MAC_MGMT_DEAUTH) && (pMac->sys.gSysFrameCount[type][subType] >= MAX_DEAUTH_ALLOWED))
-            {
-                tANI_U32 timeNow = adf_os_ticks();
-                tANI_U32 timeGap = adf_os_ticks_to_msecs(timeNow -
-                                              lastDeauthPacketTime);
-                if (timeGap < 1000) {
-#ifdef WLAN_FEATURE_11W
-                    pMacHdr = WDA_GET_RX_MAC_HEADER(pBd);
-                    psessionEntry = peFindSessionByPeerSta(pMac,
-                                        pMacHdr->sa, &sessionId);
-                    if(!psessionEntry) {
-                        PELOGE(sysLog(pMac, LOGE,
-                            FL("session does not exist for given STA [%pM]"),
-                            pMacHdr->sa););
-                        goto fail;
-                    }
-                    if (!psessionEntry->limRmfEnabled)
-#endif /* WLAN_FEATURE_11W */
-                        goto fail;
-                }
-            }
+
+            if ((subType == SIR_MAC_MGMT_DEAUTH ||
+                 subType == SIR_MAC_MGMT_DISASSOC)&&
+                lim_is_deauth_diassoc_for_drop(pMac, pBd))
+                goto fail;
 
             if (subType == SIR_MAC_MGMT_DEAUTH)
             {
@@ -188,7 +171,6 @@ sysBbtProcessMessageCore(tpAniSirGlobal pMac, tpSirMsgQ pMsg, tANI_U32 type,
                        MAC_ADDR_ARRAY(pMacHdr->sa),
                        MAC_ADDR_ARRAY(pMacHdr->bssId),
                        pMac->sys.gSysFrameCount[type][subType] ););
-                lastDeauthPacketTime = adf_os_ticks();
             }
 
             if( (dropReason = limIsPktCandidateForDrop(pMac, pBd, subType)) != eMGMT_DROP_NO_DROP)
