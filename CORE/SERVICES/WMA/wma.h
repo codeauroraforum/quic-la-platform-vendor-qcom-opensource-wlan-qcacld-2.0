f0f6e7a28975ac86992f3ea99fec1d0c56705310
Chandrasekaran, Manishekar <cmshekar@qti.qualcomm.com>
2015-07-24 16:40:22 +0530

    qcacld: Start/stop logging of FW diag events and diag logs
    Start/stop logging of specific diag events and diag logs
    in the FW based on whether active debugging is needed
    by user space.
    
    Change-Id: If077bc839ef9662f97deb41d60e4b5a2b7d78fb0
    CRs-Fixed: 866691

--- CORE/SERVICES/WMA/wma.h
+++ CORE/SERVICES/WMA/wma.h
@@ -751,6 +751,9 @@ typedef struct {
 	vos_timer_t log_completion_timer;
 	uint16_t self_gen_frm_pwr;
 	bool tx_chain_mask_cck;
+
+	uint32_t num_of_diag_events_logs;
+	uint32_t *events_logs_list;
 }t_wma_handle, *tp_wma_handle;
 
 struct wma_target_cap {
@@ -1572,7 +1575,7 @@ typedef struct wma_roam_invoke_cmd
 }t_wma_roam_invoke_cmd;
 
 #ifdef REMOVE_PKT_LOG
-static inline void wma_set_wifi_start_logger(void *wma_handle,
+static inline void wma_set_wifi_start_packet_stats(void *wma_handle,
 					struct sir_wifi_start_log *start_log)
 {
 	return;
