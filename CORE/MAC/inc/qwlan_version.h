6331f33fb744069b586f262f0ddbdce28e751164
Nandini Suresh <snandini@codeaurora.org>
2015-07-29 17:59:45 -0700

    Release 4.4.22.050 msm8992 LA.BF64.1.2.9
    Release 4.4.22.050 msm8992 LA.BF64.1.2.9
    
    Change-Id: I04d6d45eff92183667f75cdaef7fa63d286b3eff

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -42,8 +42,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            4
 #define QWLAN_VERSION_PATCH            22
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            49
-#define QWLAN_VERSIONSTR               "4.4.22.049"
+#define QWLAN_VERSION_BUILD            50
+#define QWLAN_VERSIONSTR               "4.4.22.050"
 
 
 #define AR6320_REV1_VERSION             0x5000000
