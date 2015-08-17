531b8d4682bec78608005577fd308ed7e54eba20
Sandeep Puligilla <spuligil@qca.qualcomm.com>
2015-08-17 12:50:44 -0700

    qcacld-2.0: CL 1363882 - update fw common interface files
    wmi command to configure sap blacklist parameters
    
    Change-Id: I7a4fcbdcfcbe8bdc2f939b7ac6a6053f0ce88722
    CRs-Fixed: 865207

--- CORE/SERVICES/COMMON/wmi_tlv_defs.h
+++ CORE/SERVICES/COMMON/wmi_tlv_defs.h
@@ -580,6 +580,7 @@ typedef enum {
     WMITLV_TAG_STRUC_wmi_wow_enable_icmpv6_na_flt_cmd_fixed_param,
     WMITLV_TAG_STRUC_wmi_diag_event_log_config_fixed_param,
     WMITLV_TAG_STRUC_wmi_diag_event_log_supported_event_fixed_params,
+    WMITLV_TAG_STRUC_wmi_sap_set_blacklist_param_cmd_fixed_param,
 } WMITLV_TAG_ID;
 
 /*
@@ -803,8 +804,8 @@ typedef enum {
     OP(WMI_SOC_SET_HW_MODE_CMDID) \
     OP(WMI_SOC_SET_DUAL_MAC_CONFIG_CMDID) \
     OP(WMI_WOW_ENABLE_ICMPV6_NA_FLT_CMDID) \
-    OP(WMI_DIAG_EVENT_LOG_CONFIG_CMDID)
-
+    OP(WMI_DIAG_EVENT_LOG_CONFIG_CMDID) \
+    OP(WMI_SAP_SET_BLACKLIST_PARAM_CMDID)
 /*
  * IMPORTANT: Please add _ALL_ WMI Events Here.
  * Otherwise, these WMI TLV Functions will be process them.
@@ -2168,6 +2169,11 @@ WMITLV_CREATE_PARAM_STRUC(WMI_ROAM_INVOKE_CMDID);
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_ARRAY_BYTE, A_UINT8, psk, WMITLV_SIZE_VAR)
 WMITLV_CREATE_PARAM_STRUC(WMI_SAP_OFL_ENABLE_CMDID);
 
+/* SAP set blacklist param cmd */
+#define WMITLV_TABLE_WMI_SAP_SET_BLACKLIST_PARAM_CMDID(id,op,buf,len) \
+    WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_wmi_sap_set_blacklist_param_cmd_fixed_param, wmi_sap_set_blacklist_param_cmd_fixed_param, fixed_param, WMITLV_SIZE_FIX)
+WMITLV_CREATE_PARAM_STRUC(WMI_SAP_SET_BLACKLIST_PARAM_CMDID);
+
 /* APFIND Request */
 #define WMITLV_TABLE_WMI_APFIND_CMDID(id,op,buf,len) \
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_wmi_apfind_cmd_param, wmi_apfind_cmd_param, fixed_param, WMITLV_SIZE_FIX) \
@@ -2599,7 +2605,6 @@ WMITLV_CREATE_PARAM_STRUC(WMI_DIAG_EVENT_LOG_SUPPORTED_EVENTID);
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_wmi_iface_link_stats_event_fixed_param, wmi_iface_link_stats_event_fixed_param, fixed_param, WMITLV_SIZE_FIX) \
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_ARRAY_STRUC, wmi_iface_link_stats, iface_link_stats, WMITLV_SIZE_VAR) \
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_ARRAY_STRUC, wmi_wmm_ac_stats, ac, WMITLV_SIZE_VAR)
-
 WMITLV_CREATE_PARAM_STRUC(WMI_IFACE_LINK_STATS_EVENTID);
 
 /* Update Peer link stats Event */
@@ -2607,7 +2612,6 @@ WMITLV_CREATE_PARAM_STRUC(WMI_IFACE_LINK_STATS_EVENTID);
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_wmi_peer_stats_event_fixed_param, wmi_peer_stats_event_fixed_param, fixed_param, WMITLV_SIZE_FIX) \
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_ARRAY_STRUC, wmi_peer_link_stats, peer_stats, WMITLV_SIZE_VAR) \
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_ARRAY_STRUC, wmi_rate_stats, peer_rate_stats, WMITLV_SIZE_VAR)
-
 WMITLV_CREATE_PARAM_STRUC(WMI_PEER_LINK_STATS_EVENTID);
 
 /* Update radio stats Event */
