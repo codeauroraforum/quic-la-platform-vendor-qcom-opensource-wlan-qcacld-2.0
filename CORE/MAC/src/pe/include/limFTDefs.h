c0d3c52c8ae7aba19c14794babfa1ac3633cd65a
Komal Seelam <kseelam@codeaurora.org>
2015-09-24 21:19:24 -0700

    cld: Increase the MAX FTIE size
    prima to qcacld propagation
    Currently FTIE is defined as 256bytes and the size of the
    received FTIE during 11r roamng is greater than 256 bytes
    which caused crash while accessing the unallocated memory.
    Increased MAX FTIE size to 384 bytess to accomidate bigger
    size FTIE's as per specification.
    
    Change-Id: I73a0b4b4b9c7943d71231048953058dd339eb804
    CRs-Fixed: 808231

--- CORE/MAC/src/pe/include/limFTDefs.h
+++ CORE/MAC/src/pe/include/limFTDefs.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013, 2015 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -44,7 +44,7 @@
   ------------------------------------------------------------------------*/
 #define SIR_MDIE_SIZE               3 // MD ID(2 bytes), Capability(1 byte)
 #define MAX_TIDS                    8
-#define MAX_FTIE_SIZE             256 // Max size limited to 256, on acct. of IW custom events
+#define MAX_FTIE_SIZE             384 // Max size limited to 384, on acct. of IW custom events
 
 
 /*--------------------------------------------------------------------------
