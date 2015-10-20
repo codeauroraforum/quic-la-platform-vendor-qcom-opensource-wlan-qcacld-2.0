codeauroraforum/quic-la-platform-vendor-qcom-opensource-wlan-qcacld-2.0

commit 46e7bdd0e96c6f94047b0b6f38970729f7acb815

qcacld: Update the Nss when AP advertizes the Operating mode notifcation.
Send command to FW for updating the Nss when AP sends the Operating
mode notification in beacons.

Change-Id: I0274dac7da974cf1d8bf266a742adeceb8a0f101
CRs-Fixed: 816727
Git-commit: 987b8d7d86589c39560ca1eb9a7956ee8c326526

CORE/MAC/src/pe/sch/schBeaconProcess.c
@@ -532,6 +532,15 @@ static void __schBeaconProcessForSession( tpAniSirGlobal      pMac,
                       pBeacon->OperatingMode.chanWidth,
                       pStaDs->staIndex, pMh->sa);
              }
+             /* Update Nss setting */
+             if (pStaDs->vhtSupportedRxNss !=
+                     (pBeacon->OperatingMode.rxNSS + 1)) {
+                 pStaDs->vhtSupportedRxNss =
+                     (pBeacon->OperatingMode.rxNSS + 1);
+                 limSetNssChange( pMac, psessionEntry,
+                         pStaDs->vhtSupportedRxNss,
+                         pStaDs->staIndex, pMh->sa);
+             }
           }
           else if (psessionEntry->vhtCapability && pBeacon->VHTOperation.present)
           {

CORE/SERVICES/WMA/wma.c
@@ -19656,11 +19656,19 @@ static void wma_process_update_opmode(tp_wma_handle wma_handle,
 static void wma_process_update_rx_nss(tp_wma_handle wma_handle,
                                 tUpdateRxNss *update_rx_nss)
 {
-        WMA_LOGD("%s: Rx Nss = %d", __func__, update_rx_nss->rxNss);
+	struct wma_txrx_node *intr =
+		&wma_handle->interfaces[update_rx_nss->smesessionId];
+	int rxNss = update_rx_nss->rxNss;
 
-        wma_set_peer_param(wma_handle, update_rx_nss->peer_mac,
-                           WMI_PEER_NSS, update_rx_nss->rxNss,
-                           update_rx_nss->smesessionId);
+	wma_update_txrx_chainmask(wma_handle->num_rf_chains, &rxNss);
+	intr->nss = (tANI_U8) rxNss;
+	update_rx_nss->rxNss = (tANI_U32) rxNss;
+
+	WMA_LOGD("%s: Rx Nss = %d", __func__, update_rx_nss->rxNss);
+
+	wma_set_peer_param(wma_handle, update_rx_nss->peer_mac,
+			WMI_PEER_NSS, update_rx_nss->rxNss,
+			update_rx_nss->smesessionId);
 }
 
 #ifdef FEATURE_OEM_DATA_SUPPORT

commit 62e1ad24ace3f6304ef321fb359b290741a6c5c4

qcacld-2.0: Change to address the opensource propagation error
Address the propagation error caused by the
commit:46e7bdd0e96c6f94047b0b6f38970729f7acb815
due to above propagation error NSS is getting updated wrongly as 1.

Change-Id: I11adc3443c11ddf85a6b26d4428e381c5dcef9ea
CRs-Fixed: 816727
Git-commit: 854711d6ff34634e03347d9f396455381f639316

CORE/MAC/src/pe/sch/schBeaconProcess.c
@@ -595,13 +595,6 @@ static void __schBeaconProcessForSession( tpAniSirGlobal      pMac,
                 }
                 limCheckVHTOpModeChange(pMac, psessionEntry,
                         chWidth, pStaDs->staIndex, pMh->sa);
-
-                /* Update Nss also */
-                if (pStaDs->vhtSupportedRxNss != (pBeacon->OperatingMode.rxNSS + 1)) {
-                    pStaDs->vhtSupportedRxNss = pBeacon->OperatingMode.rxNSS + 1;
-                    limSetNssChange( pMac, psessionEntry, pStaDs->vhtSupportedRxNss,
-                            pStaDs->staIndex, pMh->sa);
-                }
              }
           }
        }