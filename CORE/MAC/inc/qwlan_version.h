c2bad2459f218e02b299e7868a3a5cbed215c249
Ashish kumar goswami <agoswa@codeaurora.org>
2015-12-14 15:05:32 +0530

    Release 4.2.11.3 MDM9x35
    Release 4.2.11.3 MDM9x35
    
    Change-Id: Ibcc823aec76a27a34f5a7425c94daf3075d6c016
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -39,12 +39,12 @@ BRIEF DESCRIPTION:
 ===========================================================================*/
 
 #define QWLAN_VERSION_MAJOR            4
-#define QWLAN_VERSION_MINOR            0
-#define QWLAN_VERSION_PATCH            10
+#define QWLAN_VERSION_MINOR            2
+#define QWLAN_VERSION_PATCH            11
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            236
+#define QWLAN_VERSION_BUILD            3
 
-#define QWLAN_VERSIONSTR               "4.0.10.236"
+#define QWLAN_VERSIONSTR               "4.2.11.3"
 
 #ifdef QCA_WIFI_2_0
 
