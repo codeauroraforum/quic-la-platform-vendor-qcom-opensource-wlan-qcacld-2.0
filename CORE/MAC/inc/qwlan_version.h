dd355bfe56515abb02c74acc999bc8f7ff4e2631
Akash Patel <akashp@codeaurora.org>
2016-01-08 16:12:00 -0800

    Release 4.4.31.33A
    Release 4.4.31.33A
    
    Change-Id: I44568133d9cef6ccecee9734e1bf82e6a98042ae
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -44,7 +44,7 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_EXTRA            ""
 #define QWLAN_VERSION_BUILD            33
 
-#define QWLAN_VERSIONSTR               "4.4.31.33"
+#define QWLAN_VERSIONSTR               "4.4.31.33A"
 
 
 #define AR6320_REV1_VERSION             0x5000000
