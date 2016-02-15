e31c2e08d71c2271e8b3b8874a25c4ca3d68cf96
Ashish kumar goswami <agoswa@codeaurora.org>
2016-02-15 13:14:06 +0530

    SDIO Release 4.3.22.12
    SDIO Release 4.3.22.12
    
    Change-Id: I748fb41cf3b603d7ba6ab51d97479943d41e1ca6
    CRs-fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -43,9 +43,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            22
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            11
+#define QWLAN_VERSION_BUILD            12
 
-#define QWLAN_VERSIONSTR               "4.3.22.11"
+#define QWLAN_VERSIONSTR               "4.3.22.12"
 
 
 #define AR6320_REV1_VERSION             0x5000000
