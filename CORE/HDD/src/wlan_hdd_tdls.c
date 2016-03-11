c311816aeb79845d162426189419d9a5f350ad1c
Masti, Narayanraddi <nmasti@codeaurora.org>
2016-03-11 18:05:12 +0530

    qcacld-2.0: Send Deauth and delete all TDLS stations.
    prima to qcacld-2.0 propagation
    
    Send Deauth to TDLS peers, delete TDLS stations and
    clear hdd structures if concurrency is detected.
    
    Change-Id: Icc2019d78fc2447d388afd8a0ccb290df056e5e3
    CRs-Fixed: 878382

--- CORE/HDD/src/wlan_hdd_tdls.c
+++ CORE/HDD/src/wlan_hdd_tdls.c
@@ -108,6 +108,7 @@ void wlan_hdd_tdls_disable_offchan_and_teardown_links(hdd_context_t *hddctx)
 {
 	u16 connected_tdls_peers = 0;
 	u8 staidx;
+	hddTdlsPeer_t *curr_peer;
 	hdd_adapter_t *adapter = NULL;
 
 	if (eTDLS_SUPPORT_NOT_ENABLED == hddctx->tdls_mode) {
@@ -152,19 +153,37 @@ void wlan_hdd_tdls_disable_offchan_and_teardown_links(hdd_context_t *hddctx)
 
 	for (staidx = 0; staidx < hddctx->max_num_tdls_sta;
 							staidx++) {
-		if (hddctx->tdlsConnInfo[staidx].staId) {
-			wlan_hdd_tdls_reset_peer(adapter,
+		if (!hddctx->tdlsConnInfo[staidx].staId)
+			continue;
+
+		curr_peer = wlan_hdd_tdls_find_all_peer(hddctx,
 				hddctx->tdlsConnInfo[staidx].peerMac.bytes);
-			hdd_roamDeregisterTDLSSTA(adapter,
-				hddctx->tdlsConnInfo[staidx].staId );
-			wlan_hdd_tdls_decrement_peer_count(adapter);
-			wlan_hdd_tdls_reset_peer(adapter,
-				hddctx->tdlsConnInfo[staidx].peerMac.bytes);
-			vos_mem_zero(&hddctx->tdlsConnInfo[staidx].peerMac,
-					sizeof(v_MACADDR_t));
-			hddctx->tdlsConnInfo[staidx].staId = 0;
-			hddctx->tdlsConnInfo[staidx].sessionId = 255;
-		}
+		if (!curr_peer)
+			continue;
+
+		hddLog(LOG1, FL("indicate TDLS teardown (staId %d)"),
+			curr_peer->staId);
+
+		/* Indicate teardown to supplicant */
+		wlan_hdd_tdls_indicate_teardown(
+				curr_peer->pHddTdlsCtx->pAdapter,
+				curr_peer,
+				eSIR_MAC_TDLS_TEARDOWN_UNSPEC_REASON);
+
+		/*
+		 * Del Sta happened already as part of sme_delete_all_tdls_peers
+		 * Hence clear hdd data structure.
+		 */
+		hdd_roamDeregisterTDLSSTA(adapter,
+				hddctx->tdlsConnInfo[staidx].staId);
+		wlan_hdd_tdls_decrement_peer_count(adapter);
+		wlan_hdd_tdls_reset_peer(adapter, curr_peer->peerMac);
+
+		hddctx->tdlsConnInfo[staidx].staId = 0;
+		hddctx->tdlsConnInfo[staidx].sessionId = 255;
+
+		vos_mem_zero(&hddctx->tdlsConnInfo[staidx].peerMac,
+			sizeof(v_MACADDR_t));
 	}
 	wlan_hdd_tdls_check_bmps(adapter);
 }
