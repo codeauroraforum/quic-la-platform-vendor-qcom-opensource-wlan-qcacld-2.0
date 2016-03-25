LineageOS/android_kernel_xiaomi_msm8996

qcacld-2.0: CL 1463814 update fw common interface files
adding wmi cmd for setting BT paging and WLAN P2P/STA/SAP sched interval

Change-Id: I07b885d726c12d8a9e7e8b179ad427b24a2f5db8
CRs-Fixed: 865207

CORE/SERVICES/COMMON/wmi_tlv_defs.h
@@ -687,6 +687,7 @@ typedef enum {
     WMITLV_TAG_STRUC_wmi_pdev_hw_mode_transition_event_fixed_param,
     WMITLV_TAG_STRUC_wmi_pdev_set_hw_mode_response_vdev_mac_entry,
     WMITLV_TAG_STRUC_wmi_pdev_set_mac_config_response_event_fixed_param,
+    WMITLV_TAG_STRUC_WMI_COEX_CONFIG_CMD_fixed_param,
 } WMITLV_TAG_ID;
 
 /*
@@ -962,7 +963,8 @@ typedef enum {
     OP(WMI_PDEV_SET_HW_MODE_CMDID) \
     OP(WMI_PDEV_SET_MAC_CONFIG_CMDID) \
     OP(WMI_PDEV_SET_ANTENNA_MODE_CMDID) \
-    OP(WMI_VDEV_SET_QUIET_MODE_CMDID)
+    OP(WMI_VDEV_SET_QUIET_MODE_CMDID) \
+    OP(WMI_COEX_CONFIG_CMDID)
 
 /*
  * IMPORTANT: Please add _ALL_ WMI Events Here.
@@ -2625,6 +2627,11 @@ WMITLV_CREATE_PARAM_STRUC(WMI_NLO_CONFIGURE_MAWC_CMDID);
 #define WMITLV_TABLE_WMI_EXTSCAN_CONFIGURE_MAWC_CMDID(id,op,buf,len) \
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_wmi_extscan_configure_mawc_cmd_fixed_param, wmi_extscan_configure_mawc_cmd_fixed_param, fixed_param, WMITLV_SIZE_FIX)
 WMITLV_CREATE_PARAM_STRUC(WMI_EXTSCAN_CONFIGURE_MAWC_CMDID);
+
+/* COEX config cmd */
+#define WMITLV_TABLE_WMI_COEX_CONFIG_CMDID(id,op,buf,len) \
+    WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_WMI_COEX_CONFIG_CMD_fixed_param, WMI_COEX_CONFIG_CMD_fixed_param, fixed_param, WMITLV_SIZE_FIX)
+WMITLV_CREATE_PARAM_STRUC(WMI_COEX_CONFIG_CMDID);
 
 /* bpf offload capability get cmd */
 #define WMITLV_TABLE_WMI_BPF_GET_CAPABILITY_CMDID(id,op,buf,len) \

codeauroraforum/quic-la-platform-vendor-qcom-opensource-wlan-qcacld-2.0

CORE/SERVICES/COMMON/wmi_tlv_defs.h
@@ -559,7 +559,8 @@ typedef enum {
     WMITLV_TAG_STRUC_wmi_vdev_tsf_report_event_fixed_param,
     WMITLV_TAG_STRUC_wmi_get_fw_mem_dump_fixed_param,
     WMITLV_TAG_STRUC_wmi_update_fw_mem_dump_fixed_param,
-    WMITLV_TAG_STRUC_wmi_fw_mem_dump_params
+    WMITLV_TAG_STRUC_wmi_fw_mem_dump_params,
+    WMITLV_TAG_STRUC_WMI_COEX_CONFIG_CMD_fixed_param,
 } WMITLV_TAG_ID;
 
 /*
@@ -773,7 +774,8 @@ typedef enum {
     OP(WMI_ROAM_FILTER_CMDID) \
     OP(WMI_PASSPOINT_LIST_CONFIG_CMDID) \
     OP(WMI_VDEV_TSF_TSTAMP_ACTION_CMDID) \
-    OP(WMI_GET_FW_MEM_DUMP_CMDID)
+    OP(WMI_GET_FW_MEM_DUMP_CMDID) \
+    OP(WMI_COEX_CONFIG_CMDID)
 
 /*
  * IMPORTANT: Please add _ALL_ WMI Events Here.
@@ -2171,6 +2173,11 @@ WMITLV_CREATE_PARAM_STRUC(WMI_ROAM_FILTER_CMDID);
 #define WMITLV_TABLE_WMI_VDEV_TSF_TSTAMP_ACTION_CMDID(id,op,buf,len) \
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_wmi_vdev_tsf_tstamp_action_cmd_fixed_param, wmi_vdev_tsf_tstamp_action_cmd_fixed_param, fixed_param, WMITLV_SIZE_FIX)
 WMITLV_CREATE_PARAM_STRUC(WMI_VDEV_TSF_TSTAMP_ACTION_CMDID);
+
+/* COEX config cmd */
+#define WMITLV_TABLE_WMI_COEX_CONFIG_CMDID(id,op,buf,len) \
+    WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_WMI_COEX_CONFIG_CMD_fixed_param, WMI_COEX_CONFIG_CMD_fixed_param, fixed_param, WMITLV_SIZE_FIX)
+WMITLV_CREATE_PARAM_STRUC(WMI_COEX_CONFIG_CMDID);
 
 /************************** TLV definitions of WMI events *******************************/
 