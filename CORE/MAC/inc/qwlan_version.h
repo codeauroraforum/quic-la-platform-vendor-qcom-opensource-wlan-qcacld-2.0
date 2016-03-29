1b51cd8814f34268c7ecc1ea9c3834e8b8445e23
Ashish kumar goswami <agoswa@codeaurora.org>
2016-03-29 20:22:54 +0530

    Release 4.2.25.1A MDM9X40
    Release 4.2.25.1A MDM9X40
    
    Change-Id: I92b0f11716758fb0f531c09dec0a1338f6544b2c
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -44,7 +44,7 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_EXTRA            ""
 #define QWLAN_VERSION_BUILD            1
 
-#define QWLAN_VERSIONSTR               "4.2.25.1"
+#define QWLAN_VERSIONSTR               "4.2.25.1A"
 
 
 #define AR6320_REV1_VERSION             0x5000000
